5a7470532ef08b2ecb81f1f197c32c9dfff0a13d
Sinane <sin.sim@outlook.fr>
2022-09-22 18:43:17 +0200

    feat(admin): update product form

---
+++ apps/zkshop/pages/admin/product/edit/[id].tsx
@@ -0,0 +1,56 @@
+import { GetServerSidePropsContext } from "next";
+import { useForm } from "react-hook-form";
+import { AddProductForm } from "../../../../components/admin/Products/AddProductForm";
+import client from "../../../../libs/apollo/client";
+import {
+  Product,
+  Product_By_PkDocument,
+  useEditProductMutation,
+} from "../../../../libs/apollo/generated";
+import { AddProductFormValues } from "../add";
+
+type EditProductPageProps = {
+  product: Product;
+};
+
+const EditProductPage = ({ product }: EditProductPageProps) => {
+  const { register, handleSubmit } = useForm<AddProductFormValues>({
+    defaultValues: product,
+  });
+
+  const [editProduct, { loading }] = useEditProductMutation();
+
+  const onSubmit = async (data: AddProductFormValues) => {
+    editProduct({ variables: { ...data, id: product.id } });
+  };
+
+  return (
+    <AddProductForm
+      handleSubmit={handleSubmit}
+      register={register}
+      onSubmit={onSubmit}
+    />
+  );
+};
+
+export default EditProductPage;
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const { params } = context;
+
+  if (params?.id) {
+    const { id } = params;
+    const res = await client.query({
+      query: Product_By_PkDocument,
+      variables: {
+        id,
+      },
+    });
+
+    return {
+      props: { product: res.data?.product_by_pk },
+    };
+  }
+};
