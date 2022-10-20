6710923cbcb328e02acb58e25a4d8ea1c22005d1
Sinane <sin.sim@outlook.fr>
2022-10-21 01:28:24 +0200

    fix: discount on nft holder

--- apps/zkshop/hooks/useIsAnHolder.ts
+++ apps/zkshop/hooks/useIsAnHolder.ts
@@ -3,7 +3,7 @@ import { useAppSelector } from 'store/store';
 
 export const useIsAnHolder = ({ poapId, curation }: Product) => {
   const poapIds = useAppSelector((state) => state.user.poap).map(({ event: { id } }) => id);
-  const collections = useAppSelector((state) => state.user.nfts).map((nft) => nft.contract);
+  const collections = useAppSelector((state) => state.user.nfts).map((nft) => nft.contract.address);
 
   const isAPoapHolder = poapIds.includes(poapId);
   const isAnNftHolder = collections.includes(curation?.toLowerCase());
