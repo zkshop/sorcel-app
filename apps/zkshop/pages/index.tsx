ba4e1e246a6c26df407e47854c1a39320cccc93f
Sinane <sin.sim@outlook.fr>
2022-10-30 08:57:42 +0000

    feat: ethlisbon

--- apps/zkshop/pages/index.tsx
+++ apps/zkshop/pages/index.tsx
@@ -8,11 +8,27 @@ import useUpdateThemeOnConnection from '../hooks/useUpdateThemeOnConnection';
 import { initializeApollo, addApolloState, GetProductsDocument, GetProductsQuery } from 'apollo';
 import { fetchPOAPImageList } from 'store/slices/poapImageList';
 import { useAppDispatch } from 'store/store';
-import { ProductListContainer } from 'modules';
 
 type MarketplaceProps = {
   productsQueryResult: QueryResult<GetProductsQuery>;
 };
+
+if (typeof window !== 'undefined') {
+  const iframe = document.querySelector('iframe');
+
+  // @ts-ignore
+  window.ethereum?.on('accountsChanged', (accounts: string[]) => {
+    console.log({ accounts });
+
+    if (iframe !== null) {
+      iframe.onload = () => {
+        iframe.contentWindow?.postMessage({ type: 'ADDRESS', address: accounts?.[0] || '' }, '*');
+      };
+
+      iframe.contentWindow?.postMessage({ type: 'ADDRESS', address: accounts?.[0] || '' }, '*');
+    }
+  });
+}
 
 const Marketplace = ({ productsQueryResult }: MarketplaceProps) => {
   const { data, loading, error } = productsQueryResult;
@@ -42,7 +58,8 @@ const Marketplace = ({ productsQueryResult }: MarketplaceProps) => {
     <>
       <ReactCanvasConfetti fire={isConnected} className="canvas" />
 
-      <ProductListContainer products={data?.products} />
+      {/* <ProductListContainer products={data?.products} /> */}
+      <iframe width="100%" height="600px" src="http://localhost:3000" />
     </>
   );
 };
