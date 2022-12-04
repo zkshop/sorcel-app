b6dc44e9654c2ddddbb3421684578af0c4123cf1
Malek Rezgui <m.rezgui@outlook.fr>
2022-12-04 19:58:01 +0100

    feat(functions): allow cors

--- apps/functions/api/get-paper-token.ts
+++ apps/functions/api/get-paper-token.ts
@@ -1,10 +1,11 @@
 import { TokenService } from '../domains/TokenService';
 import { PaperWalletClient } from '../infra/PaperWalletClient';
 import { VercelRequest, VercelResponse } from '@vercel/node';
+import { allowCors } from '../allowCors';
 
 const Token = TokenService(PaperWalletClient());
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
     return res.status(404).json({ error: 'Method not allowed' });
   }
@@ -21,3 +22,5 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     res.status(500).json(e);
   }
 }
+
+export default allowCors(handler);
