48446cc5e732137bb89f38b9d528a7c09258281c
Malek Rezgui <m.rezgui@outlook.fr>
2022-12-04 19:15:42 +0100

    refactor(tbw): remove unused code

--- apps/tbw-sdk/src/store/reducer/auth.ts
+++ apps/tbw-sdk/src/store/reducer/auth.ts
@@ -1,22 +1,8 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { UserAuthenticationClient } from '../../clients/auth';
-import { AuthClient, AuthData, AuthServiceType, AuthType, PaperWallet } from './auth.type';
-
-export function AuthService(client: AuthClient): AuthServiceType {
-  return {
-    loginWithEmail: async (email) => await client.login(email),
-    refresh: async () => await client.verifyUser(),
-    logout: async () => {
-      await client.logout();
-    },
-    loginWithPaper: async (code) => await client.loginWithPaper(code),
-  };
-}
+import { AuthData, AuthType, PaperWallet } from './auth.type';
 
 type AuthSliceType = AuthData & { loading: boolean; type?: AuthType };
-
-const Auth = AuthService(UserAuthenticationClient());
 
 const initialState: AuthSliceType = {
   email: null,
@@ -39,30 +25,11 @@ export const loginWithPaper = createAsyncThunk(
   async (code: string) => await getPaperWallet(code),
 );
 
-export const getCurrentUser = createAsyncThunk(
-  'auth/getCurrentUser',
-  async () => await Auth.refresh(),
-);
-
-export const logoutUser = createAsyncThunk('auth/logout', async () => {
-  await Auth.logout();
-});
-
 export const authSlice = createSlice({
   name: 'auth',
   initialState: initialState,
   reducers: {},
   extraReducers(builder) {
-    // getCurrentUser
-    builder.addCase(getCurrentUser.fulfilled, (_, action) => ({
-      ...action.payload,
-      loading: false,
-    }));
-    builder.addCase(getCurrentUser.pending, (state) => ({ ...state, loading: true }));
-    builder.addCase(getCurrentUser.rejected, (state) => ({ ...state, loading: false }));
-    builder.addCase(logoutUser.pending, () => initialState);
-
-    // loginWithPaper
     builder.addCase(loginWithPaper.pending, (state) => ({ ...state, loading: true }));
     builder.addCase(loginWithPaper.rejected, (state) => ({ ...state, loading: false }));
     builder.addCase(loginWithPaper.fulfilled, (state, action) => ({
--- apps/tbw-sdk/src/store/reducer/auth.type.ts
+++ apps/tbw-sdk/src/store/reducer/auth.type.ts
@@ -11,17 +11,3 @@ export type AuthData = {
   email: string | null;
   phoneNumber: string | null;
 };
-
-export type AuthClient = {
-  login(email: string): Promise<AuthData>;
-  logout(): Promise<void>;
-  verifyUser(): Promise<AuthData>;
-  loginWithPaper(code: string): Promise<PaperWallet>;
-};
-
-export type AuthServiceType = {
-  loginWithEmail: (email: string) => Promise<AuthData>;
-  refresh: () => Promise<AuthData>;
-  logout: () => Promise<void>;
-  loginWithPaper(code: string): Promise<PaperWallet>;
-};
