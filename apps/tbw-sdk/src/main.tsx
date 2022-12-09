1ff072de207126c2a1b9f25efcc8906efd52efea
Malek Rezgui <m.rezgui@outlook.fr>
2022-12-09 09:44:22 +0100

    feat(tracking): add posthog

--- apps/tbw-sdk/src/main.tsx
+++ apps/tbw-sdk/src/main.tsx
@@ -3,6 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { AppProvider } from './AppProvider';
+import posthog from 'posthog-js';
+
+posthog.init('phc_NL98HSOKoKpH1YhIuYCEmFgXAl9yVUaGryiwutmYaeI', {
+  api_host: 'https://eu.posthog.com',
+  session_recording: {
+    inlineStylesheet: false,
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('3shop-app') as HTMLElement).render(
   <React.StrictMode>
