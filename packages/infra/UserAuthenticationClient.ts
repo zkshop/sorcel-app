a887efbf7dcc218431ebbf5c48a227fdf96e7b32
Sinane <sin.sim@outlook.fr>
2022-12-10 13:32:42 +0100

    fix(shop): make shop work again

---
+++ packages/infra/UserAuthenticationClient.ts
@@ -0,0 +1,56 @@
+import { TokenService } from 'domains';
+import { AuthClient } from 'domains/auth/AuthClient';
+import { magicClient } from 'magic';
+import { PaperWalletClient } from './PaperWalletClient';
+
+const initialAuthData = { email: null, issuer: null, phoneNumber: null, publicAddress: null };
+
+const paper = TokenService(PaperWalletClient());
+
+export function UserAuthenticationClient(): AuthClient {
+  return {
+    verifyUser: async () => {
+      if (!magicClient) return initialAuthData;
+      const isUserLoggedIn = await magicClient.user.isLoggedIn();
+
+      if (isUserLoggedIn) return await magicClient.user.getMetadata();
+      return initialAuthData;
+    },
+
+    login: async (email) => {
+      if (!magicClient) {
+        return initialAuthData;
+      }
+
+      const didToken = await magicClient.auth.loginWithMagicLink({
+        email,
+        redirectURI: window.location.origin,
+      });
+
+      const res = await fetch('/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + didToken,
+        },
+      });
+
+      if (res.status === 200) return await magicClient.user.getMetadata();
+
+      return initialAuthData;
+    },
+
+    logout: async () => {
+      if (!magicClient) return;
+      await magicClient.user.logout();
+    },
+
+    loginWithPaper: async (code: string) => {
+      const userToken = await paper.getToken(code);
+
+      const paperWallet = await paper.getPaperWalletInfo(userToken);
+
+      return paperWallet;
+    },
+  };
+}
