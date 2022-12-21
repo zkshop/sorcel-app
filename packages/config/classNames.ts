da9e1724e9d90b3d6e4c7b6de31e8a16af329a33
Malek Rezgui <m.rezgui@outlook.fr>
2022-12-21 14:50:00 +0100

    feat(config): add custom classnames

---
+++ packages/config/classNames.ts
@@ -0,0 +1,7 @@
+const PREFIX = '3shop-';
+
+export const classnames = {
+  NAVBAR: PREFIX + 'header',
+  PRODUCT_CARD_LIST: PREFIX + 'product-card-list',
+  PRODUCT_CARD: PREFIX + 'product-card',
+};
