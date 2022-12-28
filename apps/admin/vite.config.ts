db44fe31dd12633a17c3cbfa1504af02b9d055bf
Malek Rezgui <m.rezgui@outlook.fr>
2022-12-28 13:32:52 +0100

    refactor(vite-config): add common vite config

---
+++ apps/admin/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import { createCommonConfig } from '@3shop/vite-config/vite.config.common';
+
+const envVars = [
+  'PUBLIC_FUNCTIONS_URL',
+  'PUBLIC_HASURA_API_URL',
+  'PUBLIC_MAGIC_PUBLISHABLE_KEY',
+  'SECRET_ALCHEMY',
+  'SECRET_HASURA',
+  'SECRET_MAGIC',
+  'SECRET_SUPABASE',
+];
+
+const commonConfig = createCommonConfig(envVars);
+
+export default defineConfig({
+  ...commonConfig,
+});
