be55ca9643a8d8ec7de501934d3a1feb2fa08cdb
Malek Rezgui <m.rezgui@outlook.fr>
2023-01-02 11:26:55 +0100

    refactor(layout): remove unused code

--- apps/shop/components/Layout.tsx
+++ apps/shop/components/Layout.tsx
@@ -1,5 +1,4 @@
 import { Box, MainLayout, Banner } from '@3shop/ui';
-import { useRouter } from 'next/router';
 
 import { NavBar } from './NavBar';
 
@@ -7,24 +6,10 @@ type LayoutProps = {
   children: React.ReactNode;
 };
 
-const pageWithOutGridLayout = ['/iframe'];
-
-export const Layout = ({ children, ...props }: LayoutProps) => {
-  const { asPath } = useRouter();
-  const withLayout = asPath == '/iframe';
-  const withMainLayout = !pageWithOutGridLayout.includes(asPath);
-
-  if (withLayout) {
-    return <> {children} </>;
-  }
-
-  return (
-    <Box {...props}>
-      <NavBar admin={asPath === '/admin'} />
-
-      <Banner />
-
-      {withMainLayout ? <MainLayout> {children} </MainLayout> : children}
-    </Box>
-  );
-};
+export const Layout = ({ children, ...props }: LayoutProps) => (
+  <Box {...props}>
+    <NavBar />
+    <Banner />
+    <MainLayout> {children} </MainLayout>
+  </Box>
+);
