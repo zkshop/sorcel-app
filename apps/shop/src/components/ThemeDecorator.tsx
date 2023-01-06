6ac668ce98f1f7a1829dfab73fc2f530ce036106
Malek Rezgui <m.rezgui@outlook.fr>
2023-01-06 10:58:38 +0100

    refactor(shop): delete unused files

--- apps/shop/src/components/ThemeDecorator.tsx
+++
@@ -1,9 +0,0 @@
-import { ThemeProvider } from '@3shop/ui';
-
-type ThemeDecoratorProps = {
-  children: React.ReactNode;
-};
-
-export const ThemeDecorator = ({ children }: ThemeDecoratorProps) => (
-  <ThemeProvider>{children}</ThemeProvider>
-);
