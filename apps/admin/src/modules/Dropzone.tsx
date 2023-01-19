c7b618241f8dd05633d84f3ef572c0c6fbaba31d
Sinane <sin.sim@outlook.fr>
2023-01-19 15:58:52 +0100

    feat(shop): upload app icon by file input

---
+++ apps/admin/src/modules/Dropzone.tsx
@@ -0,0 +1,44 @@
+import { Button, Box, Image } from '@chakra-ui/react';
+
+import type { ChangeEvent } from 'react';
+import { useDropzone } from 'react-dropzone';
+
+type DropzoneProps = {
+  onChange(file: ChangeEvent<HTMLInputElement> | File): void;
+  value?: File | string;
+};
+
+export const Dropzone = ({ onChange: originalOnChange, value }: DropzoneProps) => {
+  function onDrop(files: File[]) {
+    originalOnChange(files[0]);
+  }
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+  console.log({ value });
+
+  return (
+    <Box {...getRootProps()}>
+      {typeof value === 'string' && (
+        <>
+          <Image src={value} maxHeight={200} alt="product" />
+          <input {...getInputProps()} />
+          <Button>Update file</Button>
+        </>
+      )}
+      {typeof value === 'undefined' && (
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height={200}
+          border="solid 1px"
+          borderStyle="dashed"
+        >
+          <input {...getInputProps()} />
+          <p>Drag and drop your file or click on the area</p>
+        </Box>
+      )}
+      {typeof value === 'object' && value.name}
+    </Box>
+  );
+};
