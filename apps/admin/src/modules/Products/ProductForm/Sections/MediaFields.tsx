8808d7ea84921c42fdaa6ba2e167cef0990bce6e
Sinane <sin.sim@outlook.fr>
2023-01-19 15:26:38 +0100

    feat(shop):upload-photo-by-file-input

--- apps/admin/src/modules/Products/ProductForm/Sections/MediaFields.tsx
+++ apps/admin/src/modules/Products/ProductForm/Sections/MediaFields.tsx
@@ -2,20 +2,60 @@ import {
   Heading,
   FormControl,
   FormLabel,
-  InputGroup,
-  InputLeftElement,
-  Input,
   FormErrorMessage,
   Section,
-  LinkIcon,
+  Box,
+  Image,
+  Button,
 } from '@3shop/ui';
-import { useFormContext } from 'react-hook-form';
+import { useFormContext, Controller } from 'react-hook-form';
 import type { AddProductFormValues } from '../types';
+import { useDropzone } from 'react-dropzone';
+import type { ChangeEvent } from 'react';
 
+type DropzoneProps = {
+  onChange(file: ChangeEvent<HTMLInputElement> | File): void;
+  value?: File | string;
+};
+
+const Dropzone = ({ onChange: originalOnChange, value }: DropzoneProps) => {
+  function onDrop(files: File[]) {
+    originalOnChange(files[0]);
+  }
+
+  const { getRootProps, getInputProps, acceptedFiles } = useDropzone({ onDrop });
+  console.log({ acceptedFiles });
+
+  return (
+    <Box {...getRootProps()}>
+      {typeof value === 'string' && (
+        <>
+          <Image src={value} maxHeight={200} alt="product" />
+          <input {...getInputProps()} />
+          <Button>Update file</Button>
+        </>
+      )}
+      {typeof value === 'object' && value.name}
+      {typeof value === 'undefined' && (
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height={200}
+          border="solid 1px"
+          borderStyle="dashed"
+        >
+          <input {...getInputProps()} />
+          <p>Drag and drop your file or click on the area</p>
+        </Box>
+      )}
+    </Box>
+  );
+};
 export const MediaFields = () => {
   const {
-    register,
     formState: { errors },
+    control,
   } = useFormContext<AddProductFormValues>();
 
   return (
@@ -23,15 +63,17 @@ export const MediaFields = () => {
       <Heading fontSize="xl"> Media </Heading>
 
       <FormControl isInvalid={Boolean(errors.image)}>
-        <FormLabel mb={1}> Url </FormLabel>
+        <FormLabel htmlFor="image" mb={1}>
+          Image
+        </FormLabel>
+        <Controller
+          name="image"
+          control={control}
+          render={({ field: { onChange, value } }) => (
+            <Dropzone value={value} onChange={onChange} />
+          )}
+        />
 
-        <InputGroup>
-          <InputLeftElement pointerEvents="none" color="gray.300" fontSize="1.2em">
-            <LinkIcon />
-          </InputLeftElement>
-
-          <Input placeholder="Image link" {...register('image')} />
-        </InputGroup>
         <FormErrorMessage>{errors.image?.message}</FormErrorMessage>
       </FormControl>
     </Section>
