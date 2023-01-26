9a624134b13501d87e6b634bbf7e4a9c2929327b
Malek Rezgui <m.rezgui@outlook.fr>
2023-01-26 11:00:58 +0100

    feat(tbw): add ungated nft paris

--- specific-apps/tbw/vite.config.ts
+++ specific-apps/tbw/vite.config.ts
@@ -1,34 +1,53 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
 import { createCommonConfig } from '@3shop/vite-config/vite.config.common';
 
-const envVars = [
-  'APP_ID',
-  'PAPER_CLIENT_ID',
-  'PUBLIC_MAGIC_PUBLISHABLE_KEY',
-  'PUBLIC_FUNCTIONS_URL',
-  'PUBLIC_HASURA_API_URL',
-  'SECRET_ALCHEMY',
-  'SECRET_HASURA',
-  'SECRET_MAGIC',
-  'SECRET_SUPABASE',
-];
+const envVars = {
+  APP_ID: null,
+  NETWORK: undefined,
+  PAPER_CLIENT_ID: undefined,
+  PUBLIC_FUNCTIONS_URL: undefined,
+  PUBLIC_HASURA_API_URL: undefined,
+  PUBLIC_MAGIC_PUBLISHABLE_KEY: undefined,
+  SECRET_AIRTABLE: undefined,
+  SECRET_ALCHEMY: undefined,
+  SECRET_HASURA: undefined,
+  SECRET_MAGIC: undefined,
+  SECRET_SUPABASE: undefined,
+};
 
 const dirname = __dirname;
 
 const commonConfig = createCommonConfig({ envVars, dirname });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default defineConfig(({ command, mode }) => ({
-  ...commonConfig,
-  build: {
-    ...commonConfig.build,
-    rollupOptions: {
-      ...commonConfig.build?.rollupOptions,
-      output: {
-        inlineDynamicImports: true,
-        entryFileNames: '[name].js',
-        assetFileNames: '[name].[ext]',
+export default defineConfig(({ command, mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
+
+  return {
+    ...commonConfig,
+    plugins: [
+      ...commonConfig.plugins,
+      createHtmlPlugin({
+        inject: {
+          data: {
+            SET_APP_ID: `
+              <script type="text/javascript">window.__3SHOP_APP_ID__ = "${process.env.APP_ID}";</script>
+            `,
+          },
+        },
+      }),
+    ],
+    build: {
+      ...commonConfig.build,
+      rollupOptions: {
+        ...commonConfig.build?.rollupOptions,
+        output: {
+          inlineDynamicImports: true,
+          entryFileNames: '[name].js',
+          assetFileNames: '[name].[ext]',
+        },
       },
     },
-  },
-}));
+  };
+});
