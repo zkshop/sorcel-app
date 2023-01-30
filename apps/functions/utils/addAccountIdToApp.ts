44f9b81e7513dbe4971806a22afe0eac3f773baf
Sinane <sin.sim@outlook.fr>
2023-01-30 10:28:42 +0100

    feat(functions): functions to create onboarding link

--- apps/functions/utils/addAccountIdToApp.ts
+++ apps/functions/utils/addAccountIdToApp.ts
@@ -1,23 +1,19 @@
 import hasura from './hasura';
 
-export async function addAccountIdToApp(appId: string) {
-  const mutation = `
-	{
-			update_app_by_pk(pk_columns: {id: "${appId}"}) {
-					id
-					name
+export async function addAccountIdToApp(moneyAccountId: string, appId: string) {
+  const query = `
+		mutation {
+			update_app_by_pk(pk_columns: {id: "${appId}"}, _set: {moneyAccountId: "${moneyAccountId}"}) {
+				id
+				name
 			}
-	}`;
+		}`;
 
   const payload = {
-    mutation,
+    query,
   };
 
-  try {
-    const data = await hasura.mutate(payload);
+  const data = await hasura.mutate(payload);
 
-    return data?.data?.update_app_by_pk;
-  } catch (error) {
-    console.error(error);
-  }
+  return data?.data?.update_app_by_pk;
 }
--- packages/domains/money-account/MoneyAccountClient.ts
+++ packages/domains/money-account/MoneyAccountClient.ts
@@ -1,8 +1,11 @@
 export type MoneyAccount = {
   id: string;
+  status?: string[] | null;
+  requirements: any;
 };
 
 export type MoneyAccountClient = {
   createAccount(): Promise<string>;
+  createOnboardingLink(onboardingUrl: string): Promise<string>;
   getAccount(accountId: string): Promise<MoneyAccount>;
 };
