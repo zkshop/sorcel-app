9a95794870810bc7758c93ff8bc605acd4531aa0
Sinane <sin.sim@outlook.fr>
2023-02-06 15:55:47 +0100

    fix: file extension joinclassnames

--- packages/stripe/index.ts
+++ packages/stripe/index.ts
@@ -1,6 +1,8 @@
 import Stripe from 'stripe';
 import { envVars } from '@3shop/config';
 
+const API_VERSION = '2022-11-15';
+
 export const stripe = new Stripe(envVars.SECRET_STRIPE || '', {
-  apiVersion: '2022-11-15',
+  apiVersion: API_VERSION,
 });
