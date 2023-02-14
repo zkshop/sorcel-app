e9fbd83cce5044da8a33de2ae9aa4c459f48b250
Malek Rezgui <m.rezgui@outlook.fr>
2023-02-14 11:31:11 +0100

    fix(prod): quick fix buffer is not defined

--- apps/shop/src/main.tsx
+++ apps/shop/src/main.tsx
@@ -1,3 +1,11 @@
+// TODO: uncomment this before dev
+import { Buffer as BufferPolyfill } from 'buffer';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+declare let Buffer: typeof BufferPolyfill;
+globalThis.Buffer = BufferPolyfill;
+(window.global as any) = globalThis;
+// END TODO
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
