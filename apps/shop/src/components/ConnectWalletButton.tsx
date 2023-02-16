fd31d300077be803e1baacde7917deb4c78c27ce
Sinane <sin.sim@outlook.fr>
2023-02-16 15:24:17 +0100

    fix(shop): build with wallet package deps

--- apps/shop/src/components/ConnectWalletButton.tsx
+++
@@ -1,5 +0,0 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-
-const ConnectWalletButton = () => <ConnectButton label="Wallet connect" chainStatus="icon" />;
-
-export default ConnectWalletButton;
