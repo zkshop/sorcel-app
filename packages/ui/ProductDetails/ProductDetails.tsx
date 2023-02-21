4ef4fd95ed14d600c7fada0ccff024ccbe188cd7
Malek Rezgui <m.rezgui@outlook.fr>
2023-02-21 18:11:05 +0100

    Revert "feat(product-details): change buy wording"
    This reverts commit 35610b452dcf1a0bea82f80528bd81ad6e55208c.

--- packages/ui/ProductDetails/ProductDetails.tsx
+++ packages/ui/ProductDetails/ProductDetails.tsx
@@ -154,7 +154,7 @@ export const ProductDetails = ({
                   mr={1}
                   textTransform="uppercase"
                 >
-                  BUY IT NOW
+                  PAY
                 </Text>
 
                 <Box borderRadius="2xl" display="flex">
