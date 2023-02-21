d88d33beaaac5af5fab3030e388a08f7fda2cdaa
Malek Rezgui <m.rezgui@outlook.fr>
2023-02-21 15:10:54 +0100

    fix(layout): update maxWidth & grid

--- apps/shop/src/routes/Shipping.tsx
+++ apps/shop/src/routes/Shipping.tsx
@@ -17,7 +17,7 @@ export const Shipping = () => {
   return (
     <>
       <BackButton href={`/product/${product.id}`} />
-      <ShippingFormContainer product={product} />;
+      <ShippingFormContainer product={product} />
     </>
   );
 };
