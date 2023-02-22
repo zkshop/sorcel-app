30f35825ed42f05a2e6d1d098d8a314cfb4685b7
Sinane <sin.sim@outlook.fr>
2023-02-22 14:47:36 +0100

    feat(shop): send event Order Confirmation to rudderstack on payment success

--- packages/events/index.ts
+++ packages/events/index.ts
@@ -6,3 +6,18 @@ const RUDDERSTACK_URL = 'https://shopadrien.dataplane.rudderstack.com';
 export const eventClient = new RudderAnalytics(envVars.SECRET_RUDDERSTACK || '', {
   dataPlaneUrl: RUDDERSTACK_URL,
 });
+
+export const ORDER_CONFIRMATION = 'Order Confirmation';
+
+type OrderConfirmationEventData = {
+  shop_logo_url: string;
+  name: string;
+  product_name: string;
+  shop_name: string;
+  price: number;
+  img_url: string;
+};
+
+export const sendOrderConfirmation = (email: string, eventData: OrderConfirmationEventData) => {
+  eventClient.track({ event: ORDER_CONFIRMATION, userId: email, properties: eventData });
+};
