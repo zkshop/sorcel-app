c00185b63735467ce1e909303ff0103374f0ac09
Sinane <sin.sim@outlook.fr>
2023-03-30 16:30:20 +0200

    feat: poll css

---
+++ apps/poll/src/Card.tsx
@@ -0,0 +1,19 @@
+import type { WithChildren } from '@3shop/types';
+import { VStack } from '@3shop/ui';
+import {
+  CHOICE_CARD_BACKGROUND_COLOR,
+  CHOICE_CARD_BORDER_RADIUS,
+  CHOICE_CARD_COLOR,
+} from './constant';
+
+export const Card = ({ children }: WithChildren<object>) => (
+  <VStack
+    backgroundColor={CHOICE_CARD_BACKGROUND_COLOR}
+    color={CHOICE_CARD_COLOR}
+    borderRadius={CHOICE_CARD_BORDER_RADIUS}
+    paddingBottom={4}
+    overflow="hidden"
+  >
+    {children}
+  </VStack>
+);
