902a05b74280632d2407151e7d15e7c2d0c97555
Sinane <sin.sim@outlook.fr>
2023-04-10 15:20:19 +0200

    fix(poll): choices components css on different devices + title on images + go back button

--- apps/poll/src/Image.tsx
+++ apps/poll/src/Image.tsx
@@ -8,15 +8,19 @@ type ImageProps = {
 
 const DEFAULT_IMAGE = 'choices_background.png';
 
-// @ts-ignore
 const StyledImage = styled(ChakraImage)<{ isLocked: boolean }>`
-  max-height: 100%;
-  @media (min-width: 1024px) {
+  @media (max-width: 1440px) {
     max-height: 80%;
   }
 
+  @media (max-width: 1024px) {
+    max-height: 100%;
+  }
+
+  max-height: 600px;
+
   object-fit: cover;
-  opacity: ${({ isLocked }) => (isLocked ? 0.4 : 1)};
+  opacity: ${({ isLocked }) => (isLocked ? 0.25 : 1)};
 `;
 
 export const Image = ({ src = DEFAULT_IMAGE, isLocked: isLocked }: ImageProps) => (
--- apps/poll/src/Layout.tsx
+++ apps/poll/src/Layout.tsx
@@ -1,10 +1,28 @@
-import { Grid, GridItem, Image } from '@3shop/ui';
+import { Button, Grid, GridItem, Image } from '@3shop/ui';
 import { ConnectWalletButton } from '@3shop/wallet';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom';
+
+const BackButton = () => {
+  const navigate = useNavigate();
+
+  function goBack() {
+    navigate('/');
+  }
+
+  return (
+    <Button onClick={goBack} background="#383838" position="absolute" left={4} bottom={4}>
+      Back
+    </Button>
+  );
+};
 
 function Layout() {
+  const location = useLocation();
+
+  const isHome = location.pathname === '/';
+
   return (
-    <Grid h="100vh" templateRows="repeat(10, 1fr)">
+    <Grid h="100vh" templateRows="repeat(10, 1fr)" position="relative">
       <GridItem
         display="flex"
         alignItems="center"
@@ -21,6 +39,8 @@ function Layout() {
       <GridItem rowSpan={9} padding={15}>
         <Outlet />
       </GridItem>
+
+      {!isHome && <BackButton />}
     </Grid>
   );
 }
