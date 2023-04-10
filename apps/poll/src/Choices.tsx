2b9034846f4c4c5360a63f6ac3aa5f59b57d8f4c
Sinane <sin.sim@outlook.fr>
2023-04-10 15:20:19 +0200

    fix(poll): choices components css on different devices + title on images + go back button

--- apps/poll/src/Choices.tsx
+++ apps/poll/src/Choices.tsx
@@ -1,7 +1,7 @@
 import { VoteModal } from './VoteModal';
 import { useGetPollByIdQuery, useVoteMutation } from '@3shop/apollo';
 import type { Nullable } from '@3shop/types';
-import { Box, Flex, Spinner, useDisclosure, Heading, useToast } from '@3shop/ui';
+import { Box, Flex, Spinner, useDisclosure, Heading, useToast, Text } from '@3shop/ui';
 
 import { useAccount } from '@3shop/wallet';
 import { useState } from 'react';
@@ -13,6 +13,7 @@ import { isHolder } from './utils';
 import { haveAlreadyVote } from './utils/haveAlreadyVote';
 import { Image } from './Image';
 import { updateCacheAfterVote } from './cache';
+import { CHOICE_CARD_BORDER_RADIUS } from './constant';
 
 export const Choices = () => {
   const { id } = useParams() as { id: string };
@@ -64,12 +65,26 @@ export const Choices = () => {
           flex={1}
           display="flex"
           justifyContent="center"
-          alignItems="baseline"
+          alignItems="center"
           position="relative"
         >
+          <Box
+            position="absolute"
+            top="50%"
+            left="50%"
+            transform="translate(-50%, -50%)"
+            zIndex={2}
+            background="rgba(0, 0, 0, 0.69)"
+            padding={8}
+            borderRadius={CHOICE_CARD_BORDER_RADIUS}
+          >
+            <Text color="white" fontWeight="bold" textAlign="center">
+              {data.poll.title}
+            </Text>
+          </Box>
           <Image isLocked={isLocked} src={data.poll.image || undefined} />
         </Box>
-        <Box flex={1} display="flex" justifyContent="center" alignItems="baseline">
+        <Box flex={1} display="flex" justifyContent="center" alignItems="center">
           {isLocked ? (
             <Heading textAlign="center" fontSize="x-large">
               Connect your wallet with Human Divergence NFT to vote
