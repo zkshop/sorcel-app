892de2de8880b7009adca33999e295afcae6862f
Sinane <sin.sim@outlook.fr>
2023-04-12 10:12:43 +0200

    fix(poll): css for mobile version

--- apps/poll/src/Choices.tsx
+++ apps/poll/src/Choices.tsx
@@ -1,19 +1,45 @@
 import { VoteModal } from './VoteModal';
 import { useGetPollByIdQuery, useVoteMutation } from '@3shop/apollo';
 import type { Nullable } from '@3shop/types';
-import { Box, Flex, Spinner, useDisclosure, Heading, useToast, Text } from '@3shop/ui';
+import { Box, Flex, Spinner, useDisclosure, Heading, useToast, Text, styled } from '@3shop/ui';
 
 import { useAccount } from '@3shop/wallet';
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { ChoiceCard } from './ChoiceCard';
+import { ChoiceItem } from './ChoiceItem';
 import { useAppSelector } from './store/store';
 import type { ChoiceType } from './utils';
 import { isHolder } from './utils';
 import { haveAlreadyVote } from './utils/haveAlreadyVote';
-import { Image } from './Image';
+import { ChoicesImage } from './ChoicesImage';
 import { updateCacheAfterVote } from './cache';
 import { CHOICE_CARD_BORDER_RADIUS } from './constant';
+
+const TitleLayer = styled(Box)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+  background: rgba(0, 0, 0, 0.69);
+  padding: 2rem;
+  border-radius: ${CHOICE_CARD_BORDER_RADIUS};
+
+  @media (max-width: 425px) {
+    display: none;
+  }
+`;
+
+const MobileTitleLayer = styled(Text)`
+  display: none;
+
+  @media (max-width: 425px) {
+    margin-top: 2rem;
+    display: block;
+    text-align: center;
+    font-weight: bold;
+  }
+`;
 
 export const Choices = () => {
   const { id } = useParams() as { id: string };
@@ -60,39 +86,25 @@ export const Choices = () => {
 
   return (
     <>
-      <Box height="100%" display="flex" flexDir="column">
-        <Box
-          flex={1}
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          position="relative"
-        >
-          <Box
-            position="absolute"
-            top="50%"
-            left="50%"
-            transform="translate(-50%, -50%)"
-            zIndex={2}
-            background="rgba(0, 0, 0, 0.69)"
-            padding={8}
-            borderRadius={CHOICE_CARD_BORDER_RADIUS}
-          >
+      <Box height="100%">
+        <Box m="auto" position="relative">
+          <TitleLayer>
             <Text color="white" fontWeight="bold" textAlign="center">
               {data.poll.title}
             </Text>
-          </Box>
-          <Image isLocked={isLocked} src={data.poll.image || undefined} />
+          </TitleLayer>
+          <ChoicesImage isLocked={isLocked} src={data.poll.image || undefined} />
+          <MobileTitleLayer>{data.poll.title}</MobileTitleLayer>
         </Box>
-        <Box flex={1} display="flex" justifyContent="center" alignItems="center">
+        <Box marginTop={8} flex={1} display="flex" justifyContent="center" alignItems="center">
           {isLocked ? (
             <Heading textAlign="center" fontSize="x-large">
               Connect your wallet with Human Divergence NFT to vote
             </Heading>
           ) : (
-            <Flex gap={2} justifyContent="space-between" flexWrap="wrap">
+            <Flex gap={2} justifyContent="center" flexWrap="wrap" w="100%">
               {data.poll.choices.map((choice) => (
-                <ChoiceCard
+                <ChoiceItem
                   key={`choice-${choice.id}`}
                   handleClickOnChoice={handleClickOnChoice}
                   title={choice.value}
