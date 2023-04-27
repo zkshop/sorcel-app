13c8673a86015d16a7165d9015e0a7f06e4d598a
Sinane <sin.sim@outlook.fr>
2023-04-27 16:55:19 +0200

    fix: db migration delete unused columns

---
+++ apps/api/migrations/dev-3shop/1682604247276_delete-gate-column-in-product-table/up.sql
@@ -0,0 +1,6 @@
+ALTER TABLE product
+DROP COLUMN IF EXISTS "poapId",
+DROP COLUMN IF EXISTS "collection",
+DROP COLUMN IF EXISTS "curation",
+DROP COLUMN IF EXISTS "discount",
+DROP COLUMN IF EXISTS "isDiscountGated";
