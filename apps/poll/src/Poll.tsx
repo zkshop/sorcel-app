db43e510fd5c9d89774b6262546164704254e014
Sinane <sin.sim@outlook.fr>
2023-05-25 23:01:39 +0200

    feat(poll): :lipstick: ui changes to display completed status on poll card

--- apps/poll/src/Poll.tsx
+++ apps/poll/src/Poll.tsx
@@ -1,17 +1,26 @@
-import { Text, Flex } from '@3shop/ui';
+import { Text, Flex, Box } from '@3shop/ui';
 import { VoteButton } from './VoteButton';
 import { PollCard } from './PollCard';
 import { PollImage } from './PollImage';
+import { TitleLayer } from './TitleLayer';
 
 type PollType = {
   id: string;
   title: string;
   image?: string;
+  completed: boolean;
 };
 
-export const Poll = ({ title, image }: PollType) => (
+export const Poll = ({ title, image, completed }: PollType) => (
   <PollCard>
-    <PollImage isLocked={false} src={image} />
+    <Box position="relative">
+      {completed && (
+        <TitleLayer>
+          <Text>Completed</Text>
+        </TitleLayer>
+      )}
+      <PollImage completed={completed} isLocked={true} src={image} />
+    </Box>
     <Flex paddingY={2} gap={2} direction="column" justifyContent="center" alignItems="center">
       <Text textAlign="center" noOfLines={1} fontFamily="Inter" fontWeight="800">
         {title}
