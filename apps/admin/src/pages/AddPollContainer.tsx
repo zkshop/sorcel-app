26e3dab9a4781b45b8b56292e91e428b40b8f04a
Sinane <sin.sim@outlook.fr>
2023-06-01 12:09:34 +0200

    feat(admin): :sparkles: panel to create poll

--- apps/admin/src/pages/AddPollContainer.tsx
+++ apps/admin/src/pages/AddPollContainer.tsx
@@ -2,11 +2,15 @@ import { useForm, useFieldArray } from 'react-hook-form';
 import { FormValidation } from '@3shop/validation';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { AddPoll } from '../modules/Poll/AddPoll';
+import { useCreatePollMutation, useGetAdminAppQuery } from '@3shop/apollo';
+import { StorageService } from '@3shop/domains';
+import { ImageStorageClient } from '@3shop/admin-infra';
 
 export type AddPollFormValues = {
   poll: string;
   image: File;
   choices: { value: string }[];
+  gate: string;
 };
 
 const ADD_POLL_SCHEMA = FormValidation.object().shape({
@@ -21,7 +25,10 @@ const ADD_POLL_SCHEMA = FormValidation.object().shape({
     .min(2, 'At least 2 choices are required'),
 });
 
+const storage = StorageService(ImageStorageClient());
+
 export const AddPollContainer = () => {
+  const { data: appData } = useGetAdminAppQuery();
   const {
     handleSubmit,
     register,
@@ -30,6 +37,8 @@ export const AddPollContainer = () => {
   } = useForm<AddPollFormValues>({
     resolver: yupResolver(ADD_POLL_SCHEMA),
   });
+
+  const [createPoll] = useCreatePollMutation();
 
   const { append, remove, fields } = useFieldArray({ control, name: 'choices' });
 
@@ -41,8 +50,22 @@ export const AddPollContainer = () => {
     remove(index);
   };
 
-  const onSubmit = (data: AddPollFormValues) => {
-    console.log(data);
+  const onSubmit = async (data: AddPollFormValues) => {
+    if (!appData) return;
+
+    try {
+      const imageUrl = await storage.uploadPicture(data.image, `polls/${appData.app[0].id}`);
+      await createPoll({
+        variables: {
+          gate: data.gate,
+          image: imageUrl,
+          title: data.poll,
+          data: data.choices,
+        },
+      });
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
