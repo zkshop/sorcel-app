3775ef4edd7b4196323238a39f20d826c9e6eeea
Sinane <sin.sim@outlook.fr>
2023-06-06 18:07:10 +0200

    feat(admin): :lipstick: ui for customization panel

--- apps/admin/src/pages/Customization.tsx
+++ apps/admin/src/pages/Customization.tsx
@@ -6,9 +6,11 @@ import {
   Header,
   Heading,
   Input,
+  SearchSelect,
   Section,
   Text,
 } from '@3shop/ui';
+import fonts from '../assets/fonts.json';
 
 export const Customization = () => (
   <Box>
@@ -32,6 +34,15 @@ export const Customization = () => (
         <FormLabel mb={1}>Text color</FormLabel>
 
         <Input placeholder="#000000" />
+        <FormErrorMessage></FormErrorMessage>
+      </FormControl>
+
+      {/* Font */}
+
+      <FormControl>
+        <FormLabel mb={1}>Font</FormLabel>
+
+        <SearchSelect useBasicStyles name="fonts" options={fonts} />
         <FormErrorMessage></FormErrorMessage>
       </FormControl>
     </Section>
