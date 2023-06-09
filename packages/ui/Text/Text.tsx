c90276538fe705bb1a95c6f650e22b1b00e85e8d
Sinane <sin.sim@outlook.fr>
2023-06-09 18:21:26 +0200

    feat(shop): :sparkles: upload font before loading module

--- packages/ui/Text/Text.tsx
+++ packages/ui/Text/Text.tsx
@@ -5,7 +5,6 @@ import { Text as ChakraText } from '@chakra-ui/react';
 type TextProps = ChakraTextProps;
 
 export const Text = ({ children, className = '', ...props }: TextProps) => (
-  // @ts-ignore
   <ChakraText className={joinClassNames(['shop3-text', className])} {...props}>
     {children}
   </ChakraText>
