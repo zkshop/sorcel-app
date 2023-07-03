00f9b3f56333bd64e7bb2cc9bb952a7390259744
Sinane <sin.sim@outlook.fr>
2023-07-03 18:56:22 +0200

    feat: :lipstick: typography

--- packages/ui/theme/components/index.ts
+++ packages/ui/theme/components/index.ts
@@ -1,7 +1,11 @@
 import { ButtonStyle } from './Button';
+import { CheckboxStyle } from './Checkbox';
 import { InputStyle } from './Input';
+import { TextStyle } from './Text';
 
 export const components = {
   Button: ButtonStyle,
   Input: InputStyle,
+  Text: TextStyle,
+  Checkbox: CheckboxStyle,
 };
