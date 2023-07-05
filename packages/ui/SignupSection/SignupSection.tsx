f54f11145b101520eb3ae94433df83754a173acd
Sinane <sin.sim@outlook.fr>
2023-07-05 15:59:47 +0200

    feat: :lipstick: sign up layout

--- packages/ui/SignupSection/SignupSection.tsx
+++ packages/ui/SignupSection/SignupSection.tsx
@@ -1,40 +1,44 @@
-import { Divider, Heading, Link, VStack } from '@chakra-ui/react';
+import { Divider, Image, Link, VStack } from '@chakra-ui/react';
 import { Button } from '../Button/Button';
 import { FormLabel } from '../FormLabel/FormLabel';
 import { Input } from '../Input';
 import { Text } from '../Text/Text';
 
 export const SignupSection = () => (
-  <VStack>
-    <Text as="h1" variant="title">
-      Sign up
-    </Text>
-    <Button width="full" variant="secondary">
-      Continue With Google
-    </Button>
-    <Button width="full" variant="secondary">
-      Continue With Github
-    </Button>
-
-    <Divider my="4" width="full" borderColor="greyscales.350" />
-
-    <FormLabel alignSelf="flex-start">
-      <Text variant="H300" color="greyscales.450">
-        Sign up using a work email
+  <VStack height="full" justifyContent="space-between">
+    <Image src="logo.svg" />
+    <VStack width="full">
+      <Text mb="1rem" as="h1" variant="title">
+        Sign Up
       </Text>
-    </FormLabel>
-    <Input placeholder="elon@tesla.com" />
-    <Button width="full" variant="primary">
-      Continue with email
-    </Button>
+      <Button width="full" variant="secondary">
+        Continue With Google
+      </Button>
+      <Button width="full" variant="secondary">
+        Continue With Github
+      </Button>
 
-    <Divider my="4" width="full" borderColor="greyscales.350" />
+      <Divider my="4" width="full" borderColor="greyscales.350" />
 
-    <Text variant="H500" color="greyscales.450">
-      Already have an account ?{' '}
-      <Link color="black" textDecoration="underline">
-        Log in
-      </Link>
-    </Text>
+      <FormLabel alignSelf="flex-start">
+        <Text variant="H300" color="greyscales.450">
+          Sign up using a work email
+        </Text>
+      </FormLabel>
+      <Input placeholder="elon@tesla.com" />
+      <Button width="full" variant="primary">
+        Continue with email
+      </Button>
+
+      <Divider my="4" width="full" borderColor="greyscales.350" />
+
+      <Text variant="H500" color="greyscales.450">
+        Already have an account ?{' '}
+        <Link color="black" textDecoration="underline">
+          Log in
+        </Link>
+      </Text>
+    </VStack>
+    <Text variant="small">Sorcel © 2023</Text>
   </VStack>
 );
