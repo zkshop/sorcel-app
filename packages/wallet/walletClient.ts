bdc92fafdfc26b1eb232614f17bcb277527464ea
Sinane <sin.sim@outlook.fr>
2023-07-11 10:33:51 +0200

    fix: build

--- packages/wallet/walletClient.ts
+++ packages/wallet/walletClient.ts
@@ -1,22 +1,23 @@
 import { envVars } from '@3shop/config';
 import { getDefaultWallets } from '@rainbow-me/rainbowkit';
-import { configureChains, createClient } from 'wagmi';
+import { configureChains, createConfig } from 'wagmi';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 import { mainnet, polygon } from 'wagmi/chains';
 
-export const { chains, provider } = configureChains(
+export const { chains, publicClient } = configureChains(
   [mainnet, polygon],
   [alchemyProvider({ apiKey: envVars.SECRET_ALCHEMY || '' }), publicProvider()],
 );
 
 const { connectors } = getDefaultWallets({
-  appName: '3shop',
+  appName: 'Sorcel',
+  projectId: 'Sorcel',
   chains,
 });
 
-export const walletClient = createClient({
+export const walletConfig = createConfig({
   autoConnect: true,
   connectors,
-  provider,
+  publicClient,
 });
