4bca71bed10f4336ddb214fedfc7b3d856a8938e
Sinane <sin.sim@outlook.fr>
2023-07-13 10:08:45 +0200

    fix(shop): :bug: forward ref of inputs

--- packages/ui/Input/Input.tsx
+++ packages/ui/Input/Input.tsx
@@ -1,6 +1,11 @@
+import { forwardRef } from 'react';
 import type { InputProps } from '@chakra-ui/react';
 import { Input as ChakraInput } from '@chakra-ui/react';
 
 type Props = InputProps;
 
-export const Input = (props: Props) => <ChakraInput {...props} borderColor="greyscales.200" />;
+export const Input = forwardRef<HTMLInputElement, Props>((props, ref) => (
+  <ChakraInput ref={ref} {...props} borderColor="greyscales.200" />
+));
+
+Input.displayName = 'Input';
