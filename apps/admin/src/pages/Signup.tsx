4f9cce45e7959b372eb1bca595ba3152153429b1
Sinane <sin.sim@outlook.fr>
2023-07-14 14:34:37 +0200

    feat: signup form validation

--- apps/admin/src/pages/Signup.tsx
+++ apps/admin/src/pages/Signup.tsx
@@ -1,10 +1,43 @@
 import { Grid, GridItem, SignupSection } from '@3shop/ui';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { FormValidation } from '@3shop/validation';
 
-export const Signup = () => (
-  <Grid minW="calc(100vw - --chakra-sizes-0.5)" minH="calc(100vh)" templateColumns="repeat(3, 1fr)">
-    <GridItem paddingX={14} paddingY={5} colSpan={1}>
-      <SignupSection />
-    </GridItem>
-    <GridItem bgColor="#D9D9D9" colSpan={2} />
-  </Grid>
-);
+type SignupFormValues = {
+  email: string;
+};
+
+const SIGNUP_SCHEMA = FormValidation.object().shape({
+  email: FormValidation.string().email().required(),
+});
+
+export const Signup = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<SignupFormValues>({
+    resolver: yupResolver(SIGNUP_SCHEMA),
+    mode: 'onBlur',
+  });
+
+  const onSubmit = async (data: SignupFormValues) => console.log(data);
+
+  return (
+    <Grid
+      minW="calc(100vw - --chakra-sizes-0.5)"
+      minH="calc(100vh)"
+      templateColumns="repeat(3, 1fr)"
+    >
+      <GridItem paddingX={14} paddingY={5} colSpan={1}>
+        <SignupSection
+          errors={errors}
+          handleSubmit={handleSubmit}
+          onSubmit={onSubmit}
+          register={register}
+        />
+      </GridItem>
+      <GridItem bgColor="#D9D9D9" colSpan={2} />
+    </Grid>
+  );
+};
--- packages/ui/SignupSection/SignupSection.tsx
+++ packages/ui/SignupSection/SignupSection.tsx
@@ -1,14 +1,34 @@
-import { Divider, Link, VStack, Input, Image } from '@chakra-ui/react';
+import {
+  Divider,
+  Link,
+  VStack,
+  Input,
+  Image,
+  FormControl,
+  FormErrorMessage,
+} from '@chakra-ui/react';
 import { Button } from '../Button/Button';
 import { FormLabel } from '../FormLabel/FormLabel';
 // @ts-ignore
 import logo from './SORCEL_LOGO.png';
 import { Text } from '../Text/Text';
+import type { UseFormHandleSubmit, UseFormRegister, UseFormStateReturn } from 'react-hook-form';
 
-export const SignupSection = () => (
+type SignupFormValues = {
+  email: string;
+};
+
+type SignupSectionProps = {
+  onSubmit(data: SignupFormValues): Promise<void>;
+  handleSubmit: UseFormHandleSubmit<SignupFormValues>;
+  register: UseFormRegister<SignupFormValues>;
+  errors: UseFormStateReturn<SignupFormValues>['errors'];
+};
+
+export const SignupSection = ({ onSubmit, handleSubmit, register, errors }: SignupSectionProps) => (
   <VStack height="full" justifyContent="space-between">
     <Image w={32} h={12} src={logo} />
-    <VStack width="full">
+    <VStack as="form" width="full" onSubmit={handleSubmit(onSubmit)}>
       <Text mb="1rem" as="h1" variant="title">
         Sign Up
       </Text>
@@ -23,8 +43,11 @@ export const SignupSection = () => (
           Sign up using a work email
         </Text>
       </FormLabel>
-      <Input borderColor="#E5E5E5" placeholder="elon@tesla.com" />
-      <Button width="full" variant="primary">
+      <FormControl isInvalid={Boolean(errors.email)}>
+        <Input borderColor="#E5E5E5" placeholder="elon@tesla.com" {...register('email')} />
+        <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
+      </FormControl>
+      <Button width="full" variant="primary" type="submit">
         Continue with email
       </Button>
 
