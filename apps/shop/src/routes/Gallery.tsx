ea0ab75fe2b51b5f8ac3a97f83d38448b81fa5de
Sinane <sin.sim@outlook.fr>
2023-08-31 15:59:45 +0200

    refactor: :recycle: use fetch poap image

--- apps/shop/src/routes/Gallery.tsx
+++ apps/shop/src/routes/Gallery.tsx
@@ -3,10 +3,10 @@ import ReactCanvasConfetti from 'react-canvas-confetti';
 import { useAccount } from '@3shop/wallet';
 import { ProductListContainer } from '@/modules';
 import { useAppDispatch } from '@3shop/store';
-import { fetchPOAPImageList } from '@3shop/store/slices/poapImageList';
 import { envVars } from '@3shop/config';
-import { Segment_Type_Enum, useGetProductsQuery } from '@3shop/apollo';
-import { flattenDeep } from 'lodash';
+import { useGetProductsQuery } from '@3shop/apollo';
+import { useFetchPoapImageList } from '@/hooks/useFetchPoapImageList';
+import { Spinner } from '@3shop/ui';
 
 export const Gallery = () => {
   const { data, loading, error } = useGetProductsQuery({
@@ -15,29 +15,19 @@ export const Gallery = () => {
     },
   });
 
+  const getPoapImageList = useFetchPoapImageList();
+
   const { isConnected } = useAccount();
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     if (data) {
-      dispatch(
-        fetchPOAPImageList(
-          flattenDeep(
-            data.products.map((product) =>
-              product.gate.map((gate) =>
-                gate.segments
-                  .filter((segment) => segment.type === Segment_Type_Enum.Poap)
-                  .map((segment) => segment.poap_ids),
-              ),
-            ),
-          ),
-        ),
-      );
+      getPoapImageList(data);
     }
   }, [data, dispatch]);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <Spinner />;
   }
 
   if (!data || error) {
