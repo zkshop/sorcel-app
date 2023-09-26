8a33936822c7ff3963825524178523c6138748e2
Sinane <sin.sim@outlook.fr>
2023-09-26 11:28:08 +0200

    fix: add link api url to env variables

--- apps/shop/vite.config.ts
+++ apps/shop/vite.config.ts
@@ -34,6 +34,7 @@ const envVars = {
   MONTHLY_PRO_PLAN_CHECKOUT_LINK: undefined,
   YEARLY_PRO_PLAN_CHECKOUT_LINK: undefined,
   STRIPE_WEBHOOK_SECRET: false,
+  LINK_API_URL: 'https://www.walletlink.org',
 };
 
 const dirname = __dirname;
