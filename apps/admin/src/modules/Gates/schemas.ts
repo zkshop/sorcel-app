af4ee37b149e511920b9e03fee443cc3512a9354
Sinane <sin.sim@outlook.fr>
2023-09-28 14:08:51 +0200

    feat: poap selector without image

--- apps/admin/src/modules/Gates/schemas.ts
+++ apps/admin/src/modules/Gates/schemas.ts
@@ -1,28 +1,27 @@
-import * as yup from 'yup';
+import { FormValidation } from '@3shop/validation';
 
-const ADD_GATE_SCHEMA = yup.object().shape({
-  type: yup.mixed().oneOf(['NFT', 'POAP']).required(),
-  network: yup.mixed().when('type', {
+const ADD_GATE_SCHEMA = FormValidation.object().shape({
+  type: FormValidation.mixed().oneOf(['NFT', 'POAP']).required(),
+  network: FormValidation.mixed().when('type', {
     is: 'NFT',
-    then: yup.mixed().oneOf(['POLYGON', 'ETHEREUM']).required(),
+    then: FormValidation.mixed().oneOf(['POLYGON', 'ETHEREUM']).required(),
   }),
-  contractAddress: yup.string().when('type', {
+  contractAddress: FormValidation.string().when('type', {
     is: 'NFT',
-    then: yup.string().required(),
+    then: FormValidation.string().required(),
   }),
-  poapIds: yup.string().when('type', {
-    is: 'POAP',
-    then: yup
-      .string()
-      .matches(/^(\d+\s*,\s*)*\d+$/)
-      .transform((value, originalValue) => {
-        if (originalValue) {
-          return originalValue.replace(/\s*,\s*/g, ',');
-        }
-        return value;
-      })
-      .required(),
-  }),
+  poapIds: FormValidation.array()
+    .of(
+      FormValidation.object({
+        value: FormValidation.string()
+          .required('POAP ID is required')
+          .matches(
+            /^(?:\d+|https:\/\/poap\.gallery\/event\/\d+)$/,
+            'Must be a sequence of digits or a URL in the format https://poap.gallery/event/{id}',
+          ),
+      }),
+    )
+    .min(1, 'At least 1 POAP ID is required'),
 });
 
 export default ADD_GATE_SCHEMA;
