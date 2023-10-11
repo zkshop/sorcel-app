cd8d495da3f9d18baaee5ba005fdb5ff2395e14f
Sinane <sin.sim@outlook.fr>
2023-10-11 12:04:00 +0200

    feat(paywall): :sparkles: setup alchemy

--- apps/paywall/src/App.tsx
+++ apps/paywall/src/App.tsx
@@ -1,5 +1,17 @@
-import { ConnectWalletButton } from '@3shop/wallet';
+import { ConnectWalletButton, useAccount } from '@3shop/wallet';
+import { useEffect } from 'react';
 
-const App = ({}) => <ConnectWalletButton />;
+const App = ({}) => {
+  const { isConnected } = useAccount();
+
+  useEffect(() => {
+    if (isConnected) {
+      // @ts-ignore
+      window.isWalletConnected = true;
+    }
+  }, [isConnected]);
+
+  return <ConnectWalletButton />;
+};
 
 export default App;
