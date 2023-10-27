b1477cc4c73c27a03142840c3ae1d72fbf1366c6
Sinane <sin.sim@outlook.fr>
2023-10-27 10:24:46 +0200

    feat: check if user have granted acces and return the result

--- apps/functions/api/is-granted.ts
+++ apps/functions/api/is-granted.ts
@@ -3,6 +3,8 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { NftService } from '@3shop/domains';
 import { gqlRequestClient } from '@3shop/apollo';
 import { NftReaderClient } from '../infra/NftReaderClient';
+import { gateVerifier } from '../utils/matchProductGate';
+import { BAD_REQUEST, INTERNAL_SERVER_ERROR, OK } from 'http-status';
 
 const walletScrapper = NftService(NftReaderClient());
 
@@ -10,17 +12,20 @@ async function handler(req: VercelRequest, res: VercelResponse) {
   const { address, productId } = req.query as { address: string; productId: string };
 
   if (!address || !productId) {
-    return res.status(400).send('Missing address or productId');
+    return res.status(BAD_REQUEST).send('Missing address or productId');
   }
 
   const response = await gqlRequestClient.get.GetProductById({ id: productId });
 
   const nfts = await walletScrapper.getWalletNfts(address);
 
-  console.log(response);
-  console.log(nfts);
+  if (!response.product) return res.status(INTERNAL_SERVER_ERROR).send('Product not found');
 
-  return res.status(200).send({ isGranted: true });
+  const matchedGates = gateVerifier(response.product.gate, nfts);
+
+  const isGranted = matchedGates.length > 0;
+
+  return res.status(OK).send({ isGranted, address, productId });
 }
 
 export default handler;
--- specific-apps/tbw/src/modules/galery/getTargetAttribute.ts
+++
@@ -1,9 +0,0 @@
-const MEMBER_STACK_PRODUCT_ID = '51e46fb2-cd9e-4baf-81db-8df9d58b62b2';
-
-export const getTargetAttribute = (productId: string) => {
-  if (productId === MEMBER_STACK_PRODUCT_ID) {
-    return '_self';
-  }
-
-  return '_blank';
-};
