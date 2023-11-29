4d88324b56597ff2f44d396df5915d677375436b
Sinane <sin.sim@outlook.fr>
2023-11-29 11:22:02 +0100

    feat(paywall): :sparkles: pass svg through window for connect wallet button in paywall

---
+++ apps/paywall/src/assets/WalletIcon.tsx
@@ -0,0 +1,19 @@
+const WalletIcon = () => (
+  <svg
+    width={16}
+    className="wallet-icon"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 25"
+  >
+    <path
+      d="M16 9V3h-1l-1 1-9 4H4L3 9v7m14-1h0M3 12v8l1 1 2 1h12l2-1 1-1V10l-1-1H4l-1 1v2Zm14 3h-1 1Z"
+      stroke="#fff"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default WalletIcon;
