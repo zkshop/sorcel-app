4dae4bb3c5dae79c540f28064fea8a4ec437b1e6
Eclipse <eclipse@Les-MacBook-Air.local>
2023-12-05 10:55:31 +0100

    create-app GCF working locally and for deployment

---
+++ apps/functions/serverless/create-app/src/index.ts
@@ -0,0 +1,33 @@
+import { AppCreatorService } from '../../../../../packages/domains';
+import { SorcelAppCreator } from '../../../infra/SorcelAppCreator';
+
+type CreateAppParams = {
+  name: string;
+  email: string;
+};
+
+const appCreator = AppCreatorService(SorcelAppCreator());
+
+export async function createApp(req: any, res: any) {
+  const { name, email } = req.body as CreateAppParams;
+
+  if (!name || !email) {
+    return res.status(400).send('Missing name or email');
+  }
+
+  const appData = await appCreator.createApp(name);
+
+  if (!appData) {
+    console.log(appData);
+
+    return res.status(500).send('Error creating app');
+  }
+
+  const userData = await appCreator.createAdmin(email, appData.id);
+
+  if (!userData) {
+    return res.status(500).send('Error creating user');
+  }
+
+  return res.status(200).send({ appId: appData.id, name: appData.name, email: userData.email });
+}
