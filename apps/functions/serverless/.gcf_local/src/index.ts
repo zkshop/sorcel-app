5a29e937cf25a0b5c84cb0cc34511f1b82791381
shadokan87 <shadokan87@shadokan87s-MacBook-Air.local>
2023-12-07 18:12:01 +0100

    draft: everything seems to work, need to see how to handle /shop/poap query

--- apps/functions/serverless/.gcf_local/src/index.ts
+++ apps/functions/serverless/.gcf_local/src/index.ts
@@ -2,28 +2,28 @@ import express from 'express';
 
 import { createApp } from '../../create-app/src';
 import { getStripeAccount } from '../../admin/get-stripe-account/src';
-import { createStripeAccount } from '../../admin/create-stripe-account/src';
-import { login } from '../../admin/auth/login/src';
-import { updatePlan } from '../../update-plan/src';
-import { verify } from '../../admin/auth/verify/src';
 import { connectWithEmail } from '../../shop/connect-with-email/src';
 import { getPaperWallet } from '../../shop/get-paper-wallet/src';
-import { isGranted } from '../../is-granted/src';
-import { getPaperToken } from '../../shop/get-paper-token/src';
 import { paymentIntents } from '../../shop/payment-intents/src';
+import { updatePlan } from '../../update-plan/src';
+import { login } from '../../admin/auth/login/src';
+import { getPaperToken } from '../../shop/get-paper-token/src';
+import { isGranted } from '../../is-granted/src';
+import { verify } from '../../admin/auth/verify/src';
+import { createStripeAccount } from '../../admin/create-stripe-account/src';
 
 
 const app = express();
 app.use('/api/create-app', createApp);
 app.use('/api/admin/get-stripe-account', getStripeAccount);
-app.use('/api/admin/create-stripe-account', createStripeAccount);
-app.use('/api/admin/auth/login', login);
-app.use('/api/update-plan', updatePlan);
-app.use('/api/admin/auth/verify', verify);
 app.use('/api/shop/connect-with-email', connectWithEmail);
 app.use('/api/shop/get-paper-wallet', getPaperWallet);
-app.use('/api/is-granted', isGranted);
-app.use('/api/shop/get-paper-token', getPaperToken);
 app.use('/api/shop/payment-intents', paymentIntents);
+app.use('/api/update-plan', updatePlan);
+app.use('/api/admin/auth/login', login);
+app.use('/api/shop/get-paper-token', getPaperToken);
+app.use('/api/is-granted', isGranted);
+app.use('/api/admin/auth/verify', verify);
+app.use('/api/admin/create-stripe-account', createStripeAccount);
 
-export { app as index, createApp, getStripeAccount, createStripeAccount, login, updatePlan, verify, connectWithEmail, getPaperWallet, isGranted, getPaperToken, paymentIntents };
+export { app as index, createApp, getStripeAccount, connectWithEmail, getPaperWallet, paymentIntents, updatePlan, login, getPaperToken, isGranted, verify, createStripeAccount };
