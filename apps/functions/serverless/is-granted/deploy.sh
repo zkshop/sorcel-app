f181d70363ce4ef877472209adb4e79afd02c913
shadokan87 <shadokan87@shadokan87s-MacBook-Air.local>
2023-12-08 16:20:06 +0100

    added functions

---
+++ apps/functions/serverless/is-granted/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# __CF
+
+for arg in "$@"
+do
+  case $arg in
+    "--bundle")
+      pnpm bundle
+      break
+      ;;
+  esac
+done
+
+gcloud functions deploy nodejs-http-function \
+  --gen2 \
+  --runtime=nodejs20 \
+  --region=$GCP_REGION \
+  --source=. \
+  --entry-point=isGranted \
+  --trigger-http \
+  --memory=256MB \
+  --max-instances=83 \
+  --allow-unauthenticated
