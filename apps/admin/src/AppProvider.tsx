4e3debed05d14af3dd74c399a835a7bc8858a7c1
Sinane <sin.sim@outlook.fr>
2023-12-08 14:43:21 +0100

    feat(admin): :sparkles: record admin activity

--- apps/admin/src/AppProvider.tsx
+++ apps/admin/src/AppProvider.tsx
@@ -8,6 +8,7 @@ import { CookiesProvider } from 'react-cookie';
 import { CenterProvider } from '@center-inc/react';
 import { envVars } from '@3shop/config';
 import { TokenValidityProvider } from './context';
+import { PostHogProvider, posthog } from '@3shop/posthog';
 
 type AppProviderProps = {
   children: React.ReactNode;
@@ -15,23 +16,27 @@ type AppProviderProps = {
 
 const apolloClient = createApolloClient();
 
+posthog.init(envVars.POSTHOG_KEY || '');
+
 export const AppProvider = ({ children }: AppProviderProps) => (
   <React.StrictMode>
-    <TokenValidityProvider>
-      <CookiesProvider>
-        <ApolloProvider client={apolloClient}>
-          <ReduxProvider store={store}>
-            <CenterProvider
-              apiKey={envVars.SECRET_CENTER}
-              mode={envVars.NODE_ENV as 'development' | 'production'}
-            >
-              <BrowserRouter>
-                <ThemeProvider>{children}</ThemeProvider>
-              </BrowserRouter>
-            </CenterProvider>
-          </ReduxProvider>
-        </ApolloProvider>
-      </CookiesProvider>
-    </TokenValidityProvider>
+    <PostHogProvider client={posthog}>
+      <TokenValidityProvider>
+        <CookiesProvider>
+          <ApolloProvider client={apolloClient}>
+            <ReduxProvider store={store}>
+              <CenterProvider
+                apiKey={envVars.SECRET_CENTER}
+                mode={envVars.NODE_ENV as 'development' | 'production'}
+              >
+                <BrowserRouter>
+                  <ThemeProvider>{children}</ThemeProvider>
+                </BrowserRouter>
+              </CenterProvider>
+            </ReduxProvider>
+          </ApolloProvider>
+        </CookiesProvider>
+      </TokenValidityProvider>
+    </PostHogProvider>
   </React.StrictMode>
 );
