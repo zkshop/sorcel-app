8aeb5285cda3e57d0a4969a50e9160634556cdb3
Sinane <sin.sim@outlook.fr>
2023-12-08 10:41:13 +0100

    refactor: :fire: delete tbw theme

--- packages/ui-tbw/index.ts
+++
@@ -1,6 +0,0 @@
-export { ThemeProvider } from './theme/ThemeProvider';
-
-export * from './Button/Button';
-export * from './ProductCardList/ProductCardList';
-
-export { Box, Flex, VStack, HStack } from '@chakra-ui/react';
--- packages/ui-tbw/theme/theme.ts
+++
@@ -1,9 +0,0 @@
-import { extendTheme } from '@chakra-ui/react';
-
-import { breakpoints } from './breakpoints';
-import { styles } from './styles';
-
-export const theme = extendTheme({
-  ...breakpoints,
-  ...styles,
-});
