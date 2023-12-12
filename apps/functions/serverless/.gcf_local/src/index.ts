cf19899c6d5970873b09f98c41dd59475764e0eb
shadokan87 <shadokan87@shadokan87s-MacBook-Air.local>
2023-12-12 18:00:12 +0100

    draft: added openapi for every functions, yaml generation for openapi google api-gateway working, deployment working (tested with 3 functions)

--- apps/functions/serverless/.gcf_local/src/index.ts
+++ apps/functions/serverless/.gcf_local/src/index.ts
@@ -1,33 +1,33 @@
 import express from 'express';
 
-import { isGranted } from '../../is-granted/src';
-import { connectWithEmail } from '../../shop/connect-with-email/src';
-import { updatePlan } from '../../update-plan/src';
 import { createApp } from '../../create-app/src';
+import { connectWithEmail } from '../../shop/connect-with-email/src';
+import { isGranted } from '../../is-granted/src';
+import { updatePlan } from '../../update-plan/src';
 import { getPaperWallet } from '../../shop/get-paper-wallet/src';
 import { paymentIntents } from '../../shop/payment-intents/src';
-import { getStripeAccount } from '../../admin/get-stripe-account/src';
+import { address } from '../../shop/poap/address/src';
 import { id } from '../../shop/poap/events/id/src';
 import { getPaperToken } from '../../shop/get-paper-token/src';
+import { getStripeAccount } from '../../admin/get-stripe-account/src';
 import { createStripeAccount } from '../../admin/create-stripe-account/src';
 import { login } from '../../admin/auth/login/src';
-import { address } from '../../shop/poap/address/src';
 import { verify } from '../../admin/auth/verify/src';
 
 
 const app = express();
-app.use('/api/is-granted', isGranted);
-app.use('/api/shop/connect-with-email', connectWithEmail);
-app.use('/api/update-plan', updatePlan);
 app.use('/api/create-app', createApp);
+app.use('/api/shop/connect-with-email', connectWithEmail);
+app.use('/api/is-granted', isGranted);
+app.use('/api/update-plan', updatePlan);
 app.use('/api/shop/get-paper-wallet', getPaperWallet);
 app.use('/api/shop/payment-intents', paymentIntents);
-app.use('/api/admin/get-stripe-account', getStripeAccount);
+app.use('/api/shop/poap/address', address);
 app.use('/api/shop/poap/events/id', id);
 app.use('/api/shop/get-paper-token', getPaperToken);
+app.use('/api/admin/get-stripe-account', getStripeAccount);
 app.use('/api/admin/create-stripe-account', createStripeAccount);
 app.use('/api/admin/auth/login', login);
-app.use('/api/shop/poap/address', address);
 app.use('/api/admin/auth/verify', verify);
 
-export { app as index, isGranted, connectWithEmail, updatePlan, createApp, getPaperWallet, paymentIntents, getStripeAccount, id, getPaperToken, createStripeAccount, login, address, verify };
+export { app as index, createApp, connectWithEmail, isGranted, updatePlan, getPaperWallet, paymentIntents, address, id, getPaperToken, getStripeAccount, createStripeAccount, login, verify };
