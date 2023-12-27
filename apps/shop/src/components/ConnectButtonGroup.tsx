725ae85b6dee252f5306a9a74fd62b61cdafb59d
shadokan87 <shadokan87@shadokan87s-MacBook-Air.local>
2023-12-27 18:20:07 +0100

    xrp is working

--- apps/shop/src/components/ConnectButtonGroup.tsx
+++ apps/shop/src/components/ConnectButtonGroup.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { classnames } from '@3shop/config';
-import { Box, Flex, Button, Input } from '@3shop/ui';
+import { Box, Flex, Button, Input, Divider } from '@3shop/ui';
 import { ConnectWalletButton } from '@3shop/wallet';
+import { platforms } from '@3shop/domains/nft/NftPlatform';
+import { mocks } from '@3shop/domains/nft/mocks';
+import { testPlatformClient } from '@3shop/domains';
+import { testPlatformService } from '@3shop/domains';
+import { useAppSelector, useAppDispatch } from '@3shop/store';
+import { fetchPlatformNFTS } from '@3shop/store/slices/nfts';
+
 // import { fetchNFTS } from '@3shop/store/slices/nfts';
 const { useState } = React;
 
@@ -63,7 +70,53 @@ export function ConnectButtonGroup({
 
   const TestPlatform = () => {
     const [testWallet, setTestWallet] = useState('');
-    return <Input value={testWallet} onChange={(e) => setTestWallet(e.target.value)} />;
+    const [testing, setTesting] = useState(false);
+    const dispatch = useAppDispatch();
+
+    const CreateMock = () => {
+      return (
+        <>
+          {mocks.map((mock, index) => (
+            <div key={index}>
+              <Flex flexDirection="row" justifyContent="space-between">
+                <h3>{mock.name}</h3>
+                <Button
+                  onClick={() => {
+                    console.log("!!@@data",  mock.data);
+                    dispatch(fetchPlatformNFTS({ platform: mock.name, walletAddress: testWallet, identifiers: mock.data}));
+                  }}
+                >
+                  Test
+                </Button>
+              </Flex>
+              {Object.entries(mock.data).map(([key, value], i) => (
+                <div key={i}>
+                  <label>{key}</label>
+                  <Input type="text" defaultValue={value as string} />
+                </div>
+              ))}
+            </div>
+          ))}
+        </>
+      );
+    };
+    return (
+      <Box gap={0.5} flexDirection="column">
+        <Input
+          value={testWallet}
+          onChange={(e) => setTestWallet(e.target.value)}
+          placeholder="please enter wallet address"
+        />
+        <Button
+          onClick={() => {
+            setTesting(true);
+          }}
+        >
+          {'Save and test'}
+        </Button>
+        {testing && <CreateMock />}
+      </Box>
+    );
   };
   return (
     <Flex gap={1} flexDirection="row" className={classnames.GROUP_CONNECT_BUTTON}>
@@ -80,7 +133,8 @@ export function ConnectButtonGroup({
       )}
       <Flex flexDirection="column">
         <ConnectWalletButton />
-        <h1>test platforms</h1>
+        <Divider my="8" width="full" borderColor="greyscales.350" />
+        {'Test platforms'}
         <TestPlatform />
       </Flex>
     </Flex>
