fa597865ef33ad974d5ffe7710fa788577d0b412
shadokan87 <shadokan87@shadokan87s-MacBook-Air.local>
2024-01-11 11:29:04 +0100

    working on sorcelNft interface and ajusting gating logic

--- apps/shop/src/modules/shop/ProductCardContainer.tsx
+++ apps/shop/src/modules/shop/ProductCardContainer.tsx
@@ -25,30 +25,32 @@ function sortGates(a: GateFieldsFragment, b: GateFieldsFragment) {
 
 export function ProductCardContainer({ isWalletConnected, auth, product }: Props) {
   const userNFTs = useAppSelector((state) => state.user.nfts);
-  const userNFTContracts = userNFTs.map(({ contract: { address } }) => address);
-  const userPoapIds = useAppSelector((state) => state.user.poap.map((poap) => poap.event.id));
-  const poapImageList = useAppSelector((state) => state.poapImageList);
-  const gates = product.gate.slice() || [];
-  const sortedGates = gates.sort(sortGates);
+  console.log(userNFTs);
+  return <></>;
+  // const userNFTContracts = userNFTs.map(({ contract: { address } }) => address);
+  // const userPoapIds = useAppSelector((state) => state.user.poap.map((poap) => poap.event.id));
+  // const poapImageList = useAppSelector((state) => state.poapImageList);
+  // const gates = product.gate.slice() || [];
+  // const sortedGates = gates.sort(sortGates);
 
-  const matches = gateVerifier(sortedGates, userNFTs, userPoapIds);
+  // const matches = gateVerifier(sortedGates, userNFTs, userPoapIds);
 
-  const formatedProduct = formatProductData({
-    product,
-    productGates: sortedGates,
-    userPoapIds,
-    userNFTContracts,
-    matches,
-    poapImageList,
-  });
+  // const formatedProduct = formatProductData({
+  //   product,
+  //   productGates: sortedGates,
+  //   userPoapIds,
+  //   userNFTContracts,
+  //   matches,
+  //   poapImageList,
+  // });
 
-  return (
-    <GridItem
-      className={classnames.PRODUCT_CARD_LIST.GRID_ITEM}
-      display="flex"
-      justifyContent="center"
-    >
-      <ProductCard {...formatedProduct} isWalletConnected={isWalletConnected} auth={auth} />
-    </GridItem>
-  );
+  // return (
+  //   <GridItem
+  //     className={classnames.PRODUCT_CARD_LIST.GRID_ITEM}
+  //     display="flex"
+  //     justifyContent="center"
+  //   >
+  //     <ProductCard {...formatedProduct} isWalletConnected={isWalletConnected} auth={auth} />
+  //   </GridItem>
+  // );
 }
