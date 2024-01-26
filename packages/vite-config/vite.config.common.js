8811f54c0ef85aaea38a8405d1911540e7ef3f5a
Sinane <sin.sim@outlook.fr>
2024-01-26 14:39:39 +0100

    feat: :art: add unfonts to common config with updated vite version

--- packages/vite-config/vite.config.common.js
+++ packages/vite-config/vite.config.common.js
@@ -4,12 +4,19 @@ import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfil
 import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 import EnvironmentPlugin from 'vite-plugin-environment';
+import Unfonts from 'unplugin-fonts/vite';
 
 export const createCommonConfig = (options) => {
   const { dirname, envVars = [] } = options;
 
   return {
-    plugins: [react(), EnvironmentPlugin(envVars)],
+    plugins: [
+      react(),
+      EnvironmentPlugin(envVars),
+      Unfonts({
+        google: { families: [{ name: 'Inter', styles: 'wght@400;500;600;700;800;900' }] },
+      }),
+    ],
     resolve: {
       alias: {
         '@': path.resolve(dirname, './src'),
