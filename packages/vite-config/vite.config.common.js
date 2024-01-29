f84c66ba4e2057ef870e99cc544cb1ef7b34fbff
shadokan87 <shadokan87@shadokan87s-MacBook-Air.local>
2024-01-29 17:39:24 +0100

    fixed

--- packages/vite-config/vite.config.common.js
+++ packages/vite-config/vite.config.common.js
@@ -1,7 +1,5 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 import EnvironmentPlugin from 'vite-plugin-environment';
 import Unfonts from 'unplugin-fonts/vite';
@@ -16,6 +14,10 @@ export const createCommonConfig = (options) => {
       Unfonts({
         google: { families: [{ name: 'Inter', styles: 'wght@400;500;600;700;800;900' }] },
       }),
+      nodePolyfills({
+        process: true,
+        buffer: true,
+      }),
     ],
     resolve: {
       alias: {
@@ -27,16 +29,6 @@ export const createCommonConfig = (options) => {
       esbuildOptions: {
         define: {
           global: 'globalThis',
-        },
-        plugins: [
-          NodeGlobalsPolyfillPlugin({
-            process: true,
-            buffer: true,
-          }),
-          NodeModulesPolyfillPlugin(),
-        ],
-        supported: {
-          bigint: true,
         },
       },
     },
