085198c5d9bb7d783fb886bf12e18d69a44b544b
Sinane <sin.sim@outlook.fr>
2024-02-06 11:14:14 +0100

    fix(shop): :bug: path to poap cloud function

--- packages/poap/getPoapFromId.ts
+++ packages/poap/getPoapFromId.ts
@@ -3,7 +3,7 @@ import { httpServerless } from '@3shop/http-serverless';
 import type { Poap } from './Poap';
 
 export const getPOAPFromId = async (id: string) => {
-  const url = `api/shop/poap/events/id/${id}`;
+  const url = `api/shop/poap/events/${id}`;
   const { data } = await httpServerless.get<{ data: Poap }>(url);
 
   return data.data;
