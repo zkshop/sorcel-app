4159b8469875cdbfbbaa53ec999b91839cc42b4b
Sinane <sin.sim@outlook.fr>
2024-02-06 09:31:42 +0100

    fix(paywall): :bug: repair paywall environment

--- apps/functions/serverless/admin/auth/verify/src/index.ts
+++ apps/functions/serverless/admin/auth/verify/src/index.ts
@@ -4,15 +4,11 @@ import { JsonWebTokenClient } from '../../../../../infra/JsonWebTokenClient';
 import { extractTokenFromAuthorization } from '../../../../../utils';
 import type { HttpFunction } from '@google-cloud/functions-framework';
 import { envMiddleWare, allowCors } from '../../../../middlewares';
-import { envVars } from '@3shop/config';
 
 const Token = AuthorizationTokenService(JsonWebTokenClient());
 
 const handler: HttpFunction = async (req, res) => {
   const token = extractTokenFromAuthorization(req.body.didToken);
-
-  console.log('JWT: ', envVars.SECRET_JWT);
-  console.log('token: ', token);
 
   if (!token || !Token.verify(token)) return res.status(UNAUTHORIZED).end();
 
