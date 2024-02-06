5708d2af18be0f19b2a738b64800f37716bd9103
shadokan87 <shadokan87@shadokan87s-MacBook-Air.local>
2024-02-06 15:35:32 +0100

    restored .gcf_local

---
+++ apps/functions/serverless/.gcf_local/src/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+
+import { createApp } from '../../create-app/src';
+import { updatePlan } from '../../update-plan/src';
+import { getStripeAccount } from '../../admin/get-stripe-account/src';
+import { isGranted } from '../../is-granted/src';
+import { connectWithEmail } from '../../shop/connect-with-email/src';
+import { id } from '../../shop/poap/events/[id]/src';
+import { login } from '../../admin/auth/login/src';
+import { getPaperToken } from '../../shop/get-paper-token/src';
+import { paymentIntents } from '../../shop/payment-intents/src';
+import { verify } from '../../admin/auth/verify/src';
+import { getPaperWallet } from '../../shop/get-paper-wallet/src';
+import { createStripeAccount } from '../../admin/create-stripe-account/src';
+import { address } from '../../shop/poap/[address]/src';
+
+
+const app = express();
+app.use('/api/create-app', createApp);
+app.use('/api/update-plan', updatePlan);
+app.use('/api/admin/get-stripe-account', getStripeAccount);
+app.use('/api/is-granted', isGranted);
+app.use('/api/shop/connect-with-email', connectWithEmail);
+app.use('/api/shop/poap/events', id);
+app.use('/api/admin/auth/login', login);
+app.use('/api/shop/get-paper-token', getPaperToken);
+app.use('/api/shop/payment-intents', paymentIntents);
+app.use('/api/admin/auth/verify', verify);
+app.use('/api/shop/get-paper-wallet', getPaperWallet);
+app.use('/api/admin/create-stripe-account', createStripeAccount);
+app.use('/api/shop/poap', address);
+
+export { app as index, createApp, updatePlan, getStripeAccount, isGranted, connectWithEmail, id, login, getPaperToken, paymentIntents, verify, getPaperWallet, createStripeAccount, address };
