88dcfe7ea48ca4f6390f6f5468a2e2b8b16517ed
ebedoise <emilebedoiseau@gmail.com>
2024-02-07 10:27:07 +0100

    Fix email sent when not signed up

--- apps/functions/serverless/.gcf_local/src/index.ts
+++ apps/functions/serverless/.gcf_local/src/index.ts
@@ -15,14 +15,24 @@ import { login } from '../../admin/auth/login/src';
 import { address } from '../../shop/poap/[address]/src';
 import { verify } from '../../admin/auth/verify/src';
 import { getPaperWallet } from '../../shop/get-paper-wallet/src';
+import { createStripeAccount } from '../../admin/create-stripe-account/src';
+import { address } from '../../shop/poap/[address]/src';
+import { getPaperToken } from '../../shop/get-paper-token/src';
+import { login } from '../../admin/auth/login/src';
+import { verify } from '../../admin/auth/verify/src';
 
 
 const app = express();
 app.use('/api/create-app', createApp);
+<<<<<<< HEAD
+=======
+app.use('/api/is-granted', isGranted);
+>>>>>>> eed41a70 (Fix email sent when not signed up)
 app.use('/api/update-plan', updatePlan);
 app.use('/api/admin/get-stripe-account', getStripeAccount);
 app.use('/api/is-granted', isGranted);
 app.use('/api/shop/connect-with-email', connectWithEmail);
+<<<<<<< HEAD
 app.use('/api/shop/get-paper-token', getPaperToken);
 app.use('/api/shop/payment-intents', paymentIntents);
 app.use('/api/shop/poap/events', id);
@@ -32,6 +42,20 @@ app.use('/api/admin/create-stripe-account', createStripeAccount);
 app.use('/api/admin/auth/login', login);
 app.use('/api/shop/poap', address);
 app.use('/api/admin/auth/verify', verify);
+=======
+app.use('/api/shop/poap/events', id);
+app.use('/api/admin/get-stripe-account', getStripeAccount);
+app.use('/api/shop/payment-intents', paymentIntents);
+>>>>>>> eed41a70 (Fix email sent when not signed up)
 app.use('/api/shop/get-paper-wallet', getPaperWallet);
+app.use('/api/admin/create-stripe-account', createStripeAccount);
+app.use('/api/shop/poap', address);
+app.use('/api/shop/get-paper-token', getPaperToken);
+app.use('/api/admin/auth/login', login);
+app.use('/api/admin/auth/verify', verify);
 
+<<<<<<< HEAD
 export { app as index, getStripeAccount, updatePlan, createApp, connectWithEmail, createStripeAccount, getPaperWallet, login, getPaperToken, verify, paymentIntents, id, isGranted, address };
+=======
+export { app as index, createApp, isGranted, updatePlan, connectWithEmail, id, getStripeAccount, paymentIntents, getPaperWallet, createStripeAccount, address, getPaperToken, login, verify };
+>>>>>>> eed41a70 (Fix email sent when not signed up)
