2ca9c8ece5189b0541477b3ff00b5d8da948696a
shadokan87 <shadokan87@shadokan87s-MacBook-Air.local>
2024-02-07 15:03:10 +0100

    draft: not working

---
+++ apps/functions/serverless/gcf_local_orig/index.ts
@@ -0,0 +1,41 @@
+import express from 'express';
+
+import { createApp } from '../../create-app/src';
+import { isGranted } from '../../is-granted/src';
+import { getStripeAccount } from '../../admin/get-stripe-account/src';
+import { connectWithEmail } from '../../shop/connect-with-email/src';
+import { login } from '../../admin/auth/login/src';
+import { verify } from '../../admin/auth/verify/src';
+import { updatePlan } from '../../update-plan/src';
+import { getPaperWallet } from '../../shop/get-paper-wallet/src';
+import { createStripeAccount } from '../../admin/create-stripe-account/src';
+import { getPaperToken } from '../../shop/get-paper-token/src';
+import { paymentIntents } from '../../shop/payment-intents/src';
+
+const app = express();
+app.use('/create-app', createApp);
+app.use('/is-granted', isGranted);
+app.use('/get-stripe-account', getStripeAccount);
+app.use('/connect-with-email', connectWithEmail);
+app.use('/login', login);
+app.use('/verify', verify);
+app.use('/update-plan', updatePlan);
+app.use('/get-paper-wallet', getPaperWallet);
+app.use('/create-stripe-account', createStripeAccount);
+app.use('/get-paper-token', getPaperToken);
+app.use('/payment-intents', paymentIntents);
+
+export {
+  app as index,
+  createApp,
+  isGranted,
+  getStripeAccount,
+  connectWithEmail,
+  login,
+  verify,
+  updatePlan,
+  getPaperWallet,
+  createStripeAccount,
+  getPaperToken,
+  paymentIntents,
+};
--- apps/functions/serverless/shop/xumm/signin/src/index.ts
+++ apps/functions/serverless/shop/xumm/signin/src/index.ts
@@ -3,8 +3,9 @@ import { envMiddleWare, allowCors, withEnv } from '../../../../middlewares';
 import type { HttpFunction } from '@google-cloud/functions-framework';
 import axios from 'axios';
 
-const key = 'de7681ce-0e13-492e-807d-3b79f48c2dd9';
-const secret = '91bbf011-2a40-4349-a418-f2259ceae277';
+const key = "";
+const secret = "";
+
 const handler: HttpFunction = async (req, res) => {
   const payloadResponse = await axios.post(
     'https://xumm.app/api/v1/platform/payload',
