e5fd0f4eafb203e4c39911b19a04886681d67c74
Sinane <sin.sim@outlook.fr>
2024-02-08 11:15:42 +0100

    fix(shop): :bug: typo on getPOAP request parameters

--- packages/poap/getPoapFromId.ts
+++ packages/poap/getPoapFromId.ts
@@ -4,7 +4,8 @@ import type { Poap } from './Poap';
 
 export const getPOAPFromId = async (id: string) => {
   const url = `api/shop/poap/events`;
-  const { data } = await httpServerless.get<{ data: Poap }>(url, { params: id });
+
+  const { data } = await httpServerless.get<{ data: Poap }>(url, { params: { id } });
 
   return data.data;
 };
