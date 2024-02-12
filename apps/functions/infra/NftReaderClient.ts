e3444196de7b60162d26aba9898a5535c45814e9
Sinane <sin.sim@outlook.fr>
2024-02-12 16:09:08 +0100

    feat(paywall): :children_crossing: filter wallet's nft scan by contractAddresses that are already in gates

--- apps/functions/infra/NftReaderClient.ts
+++ apps/functions/infra/NftReaderClient.ts
@@ -4,8 +4,9 @@ import type { NFT, BlockchainClient } from '@3shop/domains';
 export function NftReaderClient(): BlockchainClient {
   const api = createAlchemy('ETHEREUM');
   return {
-    getWalletNfts: async (walletAddress) => {
+    getWalletNfts: async (walletAddress, contractAddresses) => {
       const result = await api.nft.getNftsForOwner(walletAddress, {
+        contractAddresses,
         excludeFilters: [NftFilters.SPAM, NftFilters.AIRDROPS],
       });
       return result.ownedNfts as NFT[];
