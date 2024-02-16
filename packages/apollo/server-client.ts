82930e0cc198c5d22113280cf748e98f7efcb004
Sinane <sin.sim@outlook.fr>
2024-02-16 12:18:17 +0100

    fix: :ambulance: grant admin authorization to graphql backend

--- packages/apollo/server-client.ts
+++ packages/apollo/server-client.ts
@@ -1,7 +1,11 @@
 import { GraphQLClient } from 'graphql-request';
 import { getSdk } from './server-generated';
 
-export const gqlClient = new GraphQLClient(process.env.PUBLIC_HASURA_API_URL || '');
+export const gqlClient = new GraphQLClient(process.env.PUBLIC_HASURA_API_URL || '', {
+  headers: {
+    'x-hasura-admin-secret': process.env.SECRET_HASURA || '',
+  },
+});
 
 export const get = getSdk(gqlClient);
 
