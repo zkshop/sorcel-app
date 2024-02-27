44b8cd5afb14913f9c6a868507e7b3254d775e9f
shadokan87 <shadokan87@shadokan87s-MacBook-Air.local>
2024-02-27 14:54:11 +0100

    fix: :bug: making mutation with axios is working

--- apps/functions/infra/SorcelAppCreator.ts
+++ apps/functions/infra/SorcelAppCreator.ts
@@ -1,7 +1,7 @@
 import type { AppCreator } from '@3shop/domains';
 import { createApp } from '../utils/createApp';
 import { createAdmin } from '../utils/createAdmin';
-import { gqlRequestClient } from '@3shop/apollo';
+import { createExampleProducts } from '../utils/createExampleProducts';
 
 export function SorcelAppCreator(): AppCreator {
   return {
@@ -10,14 +10,13 @@ export function SorcelAppCreator(): AppCreator {
 
       return response || null;
     },
-
     createExampleProducts: async (appId: string) => {
-      const response = await gqlRequestClient.get.createExampleProducts({
-        app_id: appId,
-        image_url:
-          'https://kqjytgxbtetzewipikax.supabase.co/storage/v1/object/public/products/examples/bored_ape.png?t=2024-02-20T14%3A32%3A25.997Z',
-        image_url_2:
-          'https://kqjytgxbtetzewipikax.supabase.co/storage/v1/object/public/products/examples/somaverse.png?t=2024-02-20T16%3A03%3A11.986Z',
+      const response = await createExampleProducts(
+        appId,
+        'https://kqjytgxbtetzewipikax.supabase.co/storage/v1/object/public/products/examples/bored_ape.png?t=2024-02-20T14%3A32%3A25.997Z',
+        'https://kqjytgxbtetzewipikax.supabase.co/storage/v1/object/public/products/examples/somaverse.png?t=2024-02-20T16%3A03%3A11.986Z',
+      ).catch((e) => {
+        console.log('[!ERRR]: ', e);
       });
 
       return response ? true : false;
