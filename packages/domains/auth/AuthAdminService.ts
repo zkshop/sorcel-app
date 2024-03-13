6193ae4c1357deeaddaeb2e91284dea3b91b70ae
shadokan87 <shinobi87@live.fr>
2024-03-13 12:06:13 +0100

    fix: :bug: now using logout function of magic

--- packages/domains/auth/AuthAdminService.ts
+++ packages/domains/auth/AuthAdminService.ts
@@ -1,12 +1,14 @@
 import type { AuthAdminClient, AuthAdminData } from './AuthAdminClient';
 
 type AuthAdminServiceType = {
+  logout: () => Promise<boolean>;
   login: (email: string) => Promise<AuthAdminData>;
   loginRedirect: (email: string) => Promise<boolean>;
   loginWithCredential: (credential: string) => Promise<string | null>;
 };
 
 export const AuthAdminService = (client: AuthAdminClient): AuthAdminServiceType => ({
+  logout: () => client.logout(),
   login: (email) => client.login(email),
   loginRedirect: (email) => client.loginRedirect(email),
   loginWithCredential: (crendential) => client.loginWithCredential(crendential)
