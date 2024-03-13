35a5b84d98e581d9264b3328ed3956f23e71c3b6
shadokan87 <shinobi87@live.fr>
2024-03-13 12:06:13 +0100

    fix: :bug: now using logout function of magic

--- apps/admin/src/pages/ProtectedRoutes.tsx
+++ apps/admin/src/pages/ProtectedRoutes.tsx
@@ -1,15 +1,28 @@
 import { Outlet, useNavigate } from 'react-router-dom';
-import { SidebarWithHeader, Spinner } from '@3shop/ui';
+import { SidebarWithHeader, Spinner, useToastMessage } from '@3shop/ui';
 import { useVerifyToken } from '../useVerifyToken';
 import { removeCustomerTokenCookie } from '../useCustomerTokenCookie';
+import { CustomerAuthClient } from '@3shop/admin-infra';
+import { AuthAdminService } from '@3shop/domains';
+const auth = AuthAdminService(CustomerAuthClient());
 
 export const ProtectedRoutes = () => {
   const { loading, user, email } = useVerifyToken(true);
+  const toast = useToastMessage();
   const navigate = useNavigate();
 
   function signOut() {
-    removeCustomerTokenCookie();
-    navigate('/');
+    (async () => {
+      try {
+        await auth.logout().then(() => {
+          removeCustomerTokenCookie();
+          navigate('/');
+        });
+      } catch (e) {
+        toast.error('We failed to sign you out. Please try again or contact support.');
+        console.error('Logout error:', e);
+      }
+    })();
   }
 
   return (
