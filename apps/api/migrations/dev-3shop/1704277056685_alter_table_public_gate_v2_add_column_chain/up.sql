ea3f4e9f34b6564bb088f37f4efc376443799b34
shadokan87 <shinobi87@live.fr>
2024-03-15 17:09:05 +0100

    fix: :ambulance: added condition on migration for github action error

--- apps/api/migrations/dev-3shop/1704277056685_alter_table_public_gate_v2_add_column_chain/up.sql
+++ apps/api/migrations/dev-3shop/1704277056685_alter_table_public_gate_v2_add_column_chain/up.sql
@@ -1,2 +1,7 @@
-alter table "public"."gate_v2" add column "chain" text
- not null default 'EVM';
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_attribute WHERE attrelid = 'public.gate_v2'::regclass AND attname = 'chain' AND NOT attisdropped) THEN
+        ALTER TABLE "public"."gate_v2" ADD COLUMN "chain" text NOT NULL DEFAULT 'EVM';
+    END IF;
+END
+$$;
