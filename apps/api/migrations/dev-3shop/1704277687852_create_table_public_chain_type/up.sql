dc03bb36dd4e5bceede1248e13e816354a2ed844
shadokan87 <shinobi87@live.fr>
2024-03-15 17:15:21 +0100

    fix: :ambulance: added condition on migration for github action error

--- apps/api/migrations/dev-3shop/1704277687852_create_table_public_chain_type/up.sql
+++ apps/api/migrations/dev-3shop/1704277687852_create_table_public_chain_type/up.sql
@@ -1,1 +1,8 @@
-CREATE TABLE "public"."chain_type" ("value" text NOT NULL DEFAULT 'EVM', PRIMARY KEY ("value") );
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_class WHERE relname = 'chain_type' AND relkind = 'r') THEN
+        CREATE TABLE "public"."chain_type" ("value" text NOT NULL DEFAULT 'EVM', PRIMARY KEY ("value") );
+    END IF;
+END
+$$;
+
