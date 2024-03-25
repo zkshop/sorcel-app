bc2eed3bbe097ef7ba700725d0ce5d7d91992dd7
shadokan87 <shinobi87@live.fr>
2024-03-25 16:15:39 +0100

    feat: :sparkles: added syntax highlight and XRP Ledger in dropdown

--- apps/admin/src/modules/Resources/IntegrationCode.tsx
+++ apps/admin/src/modules/Resources/IntegrationCode.tsx
@@ -2,10 +2,12 @@ import { useGetAdminAppQuery } from '@3shop/apollo';
 import { Spinner, Box, Select, Button, Code, CopyIcon, Text, VStack } from '@3shop/ui';
 import { useState } from 'react';
 import { useClipboard } from '@chakra-ui/react';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { irBlack } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-type Network = 'POLYGON' | 'ETHEREUM';
+type Network = 'POLYGON' | 'ETHEREUM' | 'XRPLEDGER';
 
-const CodeBlock = ({ code }: { code: string }) => {
+const CodeBlock = ({ code }: { code: string; language: string }) => {
   const { hasCopied, onCopy } = useClipboard(code);
 
   return (
@@ -14,7 +16,9 @@ const CodeBlock = ({ code }: { code: string }) => {
         {hasCopied ? 'Copied !' : <CopyIcon />}
       </Button>
       <Code display="block" whiteSpace="pre-wrap" backgroundColor="black" color="white">
-        {code}
+        <SyntaxHighlighter language="html" style={irBlack} wrapLongLines>
+          {code}
+        </SyntaxHighlighter>
       </Code>
     </Box>
   );
@@ -53,16 +57,17 @@ export const IntegrationCode = () => {
               <Select maxW="200px" value={network} onChange={handleChange}>
                 <option value="POLYGON">Polygon</option>
                 <option value="ETHEREUM">Ethereum</option>
+                <option value="XRPLEDGER">XRP Ledger</option>
               </Select>
             </Box>
             <br />
-            <CodeBlock code={sampleCode} />
+            <CodeBlock language="html" code={sampleCode} />
           </li>
           <li>
             Insert anywhere you want inside your &lt;body&gt; tag this div, this is where the token
             gating module will appear.
             <br />
-            <CodeBlock code='<div id="3shop-app"></div>' />
+            <CodeBlock language="html" code='<div id="3shop-app"></div>' />
           </li>
         </ol>
       </Box>
