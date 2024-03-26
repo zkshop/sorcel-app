c06cf6a2253173c99dc06bdbac2588349e67d75f
shadokan87 <shinobi87@live.fr>
2024-03-26 12:08:54 +0100

    feat: :sparkles: modal network choice

--- apps/admin/src/pages/ProtectedRoutes.tsx
+++ apps/admin/src/pages/ProtectedRoutes.tsx
@@ -4,11 +4,19 @@ import { useVerifyToken } from '../useVerifyToken';
 import { removeCustomerTokenCookie } from '../useCustomerTokenCookie';
 import { CustomerAuthClient } from '@3shop/admin-infra';
 import { AuthAdminService } from '@3shop/domains';
+import type { dialog } from '@3shop/ui/Modal/Dialogs';
+import { Dialogs } from '@3shop/ui/Modal/Dialogs';
+import { useMemo } from 'react';
+import { networkDialog } from '../modules/Dialog/firstConnection';
+import useUserSettings from '../hooks/useUserSettings';
+import React from 'react';
+
 const auth = AuthAdminService(CustomerAuthClient());
 
 export const ProtectedRoutes = () => {
   const { loading, user, email } = useVerifyToken(true);
   const toast = useToastMessage();
+  const [get, , settingsLoading] = useUserSettings();
   const navigate = useNavigate();
 
   function signOut() {
@@ -25,9 +33,19 @@ export const ProtectedRoutes = () => {
     })();
   }
 
+  const firstConnectionDialogs = useMemo<dialog[]>(() => {
+    if (settingsLoading) return [];
+    const userSettings = get();
+    if (userSettings && userSettings.network) return [];
+    return [networkDialog];
+  }, [settingsLoading]);
+
   return (
-    <SidebarWithHeader email={email} user={user} signOut={signOut}>
-      {loading ? <Spinner /> : <Outlet />}
-    </SidebarWithHeader>
+    <>
+      <Dialogs lastModalNextText="Dive In">{firstConnectionDialogs}</Dialogs>
+      <SidebarWithHeader email={email} user={user} signOut={signOut}>
+        {loading ? <Spinner /> : <Outlet />}
+      </SidebarWithHeader>
+    </>
   );
 };
