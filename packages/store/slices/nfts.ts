f261e4af88200b60c14b7c1484e00c67971bb959
shadokan87 <shinobi87@live.fr>
2024-04-03 17:28:14 +0200

    feat: :sparkles: seems to work need to check in prod if it blows capital

--- packages/store/slices/nfts.ts
+++ packages/store/slices/nfts.ts
@@ -3,25 +3,24 @@ import { NFT, NftService, SorcelNft } from '@3shop/domains';
 import { NftReaderClient, objectResolver } from '@3shop/infra';
 import { XRPNftReaderClient } from '@3shop/infra';
 import { XRPidentifers, allNames } from '@3shop/domains/nft/NftPlatform';
-import type { GetGates_V2_ByAppIdQuery } from '../../../packages/apollo';
+import { Network_Enum, type GetGates_V2_ByAppIdQuery, Segment } from '../../../packages/apollo';
 import { resolver as EVMResolver } from '@3shop/infra/EVM/resolver';
 import { resolver as XRPResolver } from '@3shop/infra/XRP/resolver';
 import { convertManyObjects } from '@3shop/infra';
 import { BithmompNft } from '@3shop/domains/nft/Xrp/Bithomp.types';
 import _ from 'lodash';
 
-const WalletScrapper = NftService(NftReaderClient());
+const NftServices = {
+  ETHEREUM: NftService(NftReaderClient(Network_Enum.Ethereum)),
+  POLYGON: NftService(NftReaderClient(Network_Enum.Polygon))
+}
 
 type Params = {
   walletAddress: string;
+  networks: Segment['network'][];
   contractAdressesToFilter: string[];
   gates: GetGates_V2_ByAppIdQuery['gates'] | undefined;
 };
-
-// const nftPlatforms = new Map<allNames, any>([
-//   ['EVM', NftReaderClient],
-//   ['XRP', XRPNftReaderClient],
-// ]);
 
 const resolvers = new Map<allNames, objectResolver<SorcelNft>>([['EVM', EVMResolver], ['XRP', XRPResolver]]);
 
@@ -30,10 +29,23 @@ export const fetchNFTS = createAsyncThunk('nfts/fetch', async (params: Params) =
   const chain = params.gates[0].chain;
   switch (chain) {
     case 'EVM': {
-      const response = await WalletScrapper.getWalletNfts(
-        params.walletAddress,
-        params.contractAdressesToFilter,
-      );
+      const polygonAdresses: string[] = [];
+      const ethAdresses: string[] = [];
+
+      for (let i = 0;i < params.networks.length; i++) {
+        if (!params.networks[i])
+          continue ;
+        if (params.networks[i] == 'POLYGON')
+          polygonAdresses.push(params.contractAdressesToFilter[i]);
+        else
+          ethAdresses.push(params.contractAdressesToFilter[i]);
+      }
+      
+      const [ethResponse, polygonResponse] = await Promise.all([
+        NftServices.ETHEREUM.getWalletNfts(params.walletAddress, ethAdresses),
+        NftServices.POLYGON.getWalletNfts(params.walletAddress, polygonAdresses),
+      ]);
+      const response = ethResponse.concat(polygonResponse);
       return convertManyObjects<NFT, SorcelNft>(response, resolvers.get(chain)!);
     } case 'XRP': {
       const queryParams: XRPidentifers[] = [...new Set(params.contractAdressesToFilter)].map(address => JSON.parse(address));
