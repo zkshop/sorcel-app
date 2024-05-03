5fc6759ec25e19e25f43cb0e8d3039eb747f27ae
ebedoise <emilebedoiseau@gmail.com>
2024-05-03 10:49:40 +0200

    :improvment integration landing page update + shopify integration button

--- apps/admin/src/pages/Integrations.tsx
+++ apps/admin/src/pages/Integrations.tsx
@@ -1,28 +1,22 @@
-import { Box, Text, HStack } from '@3shop/ui';
-import ResourceCard from '../modules/Resources/ResourcesCard';
-import { ROUTES_PATH } from '../routes/Routes';
+import { Box, VStack, Divider, Heading } from '@3shop/ui';
+import { ProductTutorial } from '../modules/Resources/ProductTutorial';
+import { IntegrationTutorial } from '../modules/Resources/IntegrationTutorial';
+import { ShopifyIntegration } from '../modules/Resources/ShopifyIntegration';
 
 export const Integrations = () => (
   <>
-    <Box>
-      <Text variant="H700" paddingY={2}>
-        Resources
-      </Text>
-      <HStack mb={2}>
-        <ResourceCard
-          link={`${ROUTES_PATH.PROTECTED.RESOURCES}/product-tutorial`}
-          title="How to create a product?"
-          description="Learn how to add products and gate them with NFTs or POAPs"
-        />
-        <ResourceCard
-          link={`${ROUTES_PATH.PROTECTED.RESOURCES}/integration-tutorial`}
-          title="How to integrate the module?"
-          description="See how to integrate the module into your website"
-        />
-      </HStack>
-      <Text variant="H700" paddingY={2}>
-        Preview of the module
-      </Text>
+    <Box padding="4" boxShadow="base" borderRadius="lg">
+      <VStack spacing="5">
+        <ProductTutorial />
+        <Box height="10px" /> 
+        <Divider borderColor="gray.400" width="full" />
+        <Box height="10px" /> 
+        <IntegrationTutorial />
+        <Box height="10px" /> 
+        <Divider borderColor="gray.400" width="full" />
+        <Box height="10px" />
+        <ShopifyIntegration />
+      </VStack>
     </Box>
   </>
 );
