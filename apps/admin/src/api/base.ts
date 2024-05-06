49791106c66c9ac5cada11f24ca4ddd4f97de7e0
shadokan87 <shinobi87@live.fr>
2024-05-06 13:37:58 +0200

    :imporvment :art: changed how backendBaseUrl is set in base.ts

--- apps/admin/src/api/base.ts
+++ apps/admin/src/api/base.ts
@@ -4,17 +4,11 @@ import axios from 'axios';
 export class Base {
   instance: AxiosInstance;
   static backendBaseUrl: string = (() => {
-    console.log(`baseurl: ${process.env.BACKEND_BASEURL}`);
-    if (process.env.BACKEND_BASEURL && process.env.BACKEND_BASEURL) {
-      console.log('case 1');
+    if (process.env.BACKEND_BASEURL && process.env.BACKEND_BASEURL)
       return process.env.BACKEND_BASEURL;
-    }
-    console.log('case 2');
     return `http://localhost:${process.env.SORCEL_DEV_BACKEND_PORT}`;
   })();
-  // static backendBaseUrl: string =
-  //   (process.env.BACKEND_BASEURL.length && process.env.BACKEND_BASEURL) ||
-  //   `http://localhost:${process.env.SORCEL_DEV_BACKEND_PORT}`;
+
   constructor(private baseUrl: string = Base.backendBaseUrl) {
     this.instance = axios.create({
       baseURL: this.baseUrl,
