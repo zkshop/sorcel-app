e3d25cca4a182f2c5e94f5dafe7d165b87ab10ac
shadokan87 <shinobi87@live.fr>
2024-05-06 12:23:15 +0200

    logging baseurl, because wrong one used in prod

--- apps/admin/src/api/base.ts
+++ apps/admin/src/api/base.ts
@@ -3,9 +3,18 @@ import axios from 'axios';
 
 export class Base {
   instance: AxiosInstance;
-  static backendBaseUrl: string =
-    (process.env.BACKEND_BASEURL.length && process.env.BACKEND_BASEURL) ||
-    `http://localhost:${process.env.SORCEL_DEV_BACKEND_PORT}`;
+  static backendBaseUrl: string = (() => {
+    console.log(`baseurl: ${process.env.BACKEND_BASEURL}`);
+    if (process.env.BACKEND_BASEURL && process.env.BACKEND_BASEURL) {
+      console.log('case 1');
+      return process.env.BACKEND_BASEURL;
+    }
+    console.log('case 2');
+    return `http://localhost:${process.env.SORCEL_DEV_BACKEND_PORT}`;
+  })();
+  // static backendBaseUrl: string =
+  //   (process.env.BACKEND_BASEURL.length && process.env.BACKEND_BASEURL) ||
+  //   `http://localhost:${process.env.SORCEL_DEV_BACKEND_PORT}`;
   constructor(private baseUrl: string = Base.backendBaseUrl) {
     this.instance = axios.create({
       baseURL: this.baseUrl,
