d6d499ea58a80e42f516636ec0836efaf4523460
shadokan87 <shinobi87@live.fr>
2024-05-28 12:14:24 +0200

    :feat :art: modified is-granted serverless function to handle multi-chain. updated env-vault, product_id in envVars taken directly from process.env instead of window

--- apps/functions/serverless/is-granted/src/index.ts
+++ apps/functions/serverless/is-granted/src/index.ts
@@ -6,8 +6,13 @@ import { BAD_REQUEST, INTERNAL_SERVER_ERROR, OK } from 'http-status';
 import { envMiddleWare, allowCors, withEnv } from '../../middlewares';
 import type { HttpFunction } from '@google-cloud/functions-framework';
 import { flatten } from 'lodash';
+import type { Segment } from '@3shop/apollo/server-generated';
+import { Network_Enum } from '@3shop/apollo/server-generated';
 
-const walletScrapper = withEnv(() => NftService(NftReaderClient()));
+const NftServices = withEnv(() => ({
+  ETHEREUM: NftService(NftReaderClient(Network_Enum.Ethereum)),
+  POLYGON: NftService(NftReaderClient(Network_Enum.Polygon)),
+}));
 
 const handler: HttpFunction = async (req, res) => {
   const { address, productId } = req.query as { address: string; productId: string };
@@ -20,13 +25,40 @@ const handler: HttpFunction = async (req, res) => {
     id: productId,
   });
 
+  const networks: Segment['network'][] = [];
+
   const contractAdressesToFilter = flatten(
     response.product?.gate.map((gate) =>
-      gate.segments.map((segment) => segment.nft_contract_address),
+      gate.segments.map((segment) => {
+        networks.push(segment.network);
+        return segment.nft_contract_address;
+      }),
     ),
   ) as string[];
 
-  const nfts = await walletScrapper.getWalletNfts(address, contractAdressesToFilter);
+  const nfts = await (async () => {
+    const chain = response.product?.gate[0].chain;
+
+    switch (chain) {
+      case 'EVM': {
+        const polygonAdresses: string[] = [];
+        const ethAdresses: string[] = [];
+
+        for (let i = 0; i < networks.length; i++) {
+          if (!networks[i]) continue;
+          if (networks[i] == 'POLYGON') polygonAdresses.push(contractAdressesToFilter[i]);
+          else ethAdresses.push(contractAdressesToFilter[i]);
+        }
+
+        return [
+          ...(await NftServices.ETHEREUM.getWalletNfts(address, ethAdresses)),
+          ...(await NftServices.POLYGON.getWalletNfts(address, polygonAdresses)),
+        ];
+      }
+      default:
+        return [];
+    }
+  })();
 
   if (!response.product) return res.status(INTERNAL_SERVER_ERROR).send('Product not found');
 
