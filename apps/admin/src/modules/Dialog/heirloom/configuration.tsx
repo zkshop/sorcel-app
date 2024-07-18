338adb05f8d44a4d35800a055b7e9850000c3718
shadokan87 <shinobi87@live.fr>
2024-07-18 17:44:47 +0200

    minor change in modal

--- apps/admin/src/modules/Dialog/heirloom/configuration.tsx
+++ apps/admin/src/modules/Dialog/heirloom/configuration.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect } from 'react';
 import { useDialog, type dialog } from '@3shop/ui/Modal/Dialogs';
-import { Box, Button, Checkbox, CopyIcon, Input, Text } from '@3shop/ui';
-import { useClipboard } from '@chakra-ui/react';
+import { Box, Checkbox, Text } from '@3shop/ui';
 
 const CopyCorsContent = () => {
-  const { hasCopied, onCopy } = useClipboard('http://test.com');
   const dialog = useDialog();
   const [isChecked, setIsChecked] = React.useState(false);
 
@@ -23,18 +21,11 @@ const CopyCorsContent = () => {
   return (
     <Box padding="20px">
       <Text marginBottom="10px">
-        When creating your lock, please copy the following CORS domain into the CORS field of your
-        form.
+        When creating your lock, please enter your website url into the CORS field of your form.
       </Text>
       <Box display="flex" alignItems="center" marginBottom="20px">
-        <Input value={process.env.HEIRLOOM_CORS} isReadOnly />
-        <Button onClick={onCopy} marginLeft="10px">
-          {hasCopied ? 'Copied !' : <CopyIcon />}
-        </Button>
-      </Box>
-      <Box display="flex" alignItems="center" marginBottom="20px">
         <Checkbox checked={isChecked} onChange={handleCheckboxChange} />
-        <Text marginLeft="10px">I have used this CORS for my lock</Text>
+        <Text marginLeft="10px">I have entered my website url as CORS for my lock</Text>
       </Box>
     </Box>
   );
