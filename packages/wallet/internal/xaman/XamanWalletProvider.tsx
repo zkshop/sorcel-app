e13714412b3685417fe9961a19d8b3e09766397f
ebedoise <emilebedoiseau@gmail.com>
2024-07-29 15:38:52 +0200

    now using backend for xumm signin

--- packages/wallet/internal/xaman/XamanWalletProvider.tsx
+++ packages/wallet/internal/xaman/XamanWalletProvider.tsx
@@ -112,7 +112,7 @@ export const XamanWalletProvider = ({ children }: XamanWalletProviderProps) => {
         // console.log("Sending payment request:", state.payment);
         const request = state.payment
           ? httpServerless.post<XummPostPayloadResponse>(`api/shop/xaman/payment`, { ...state.payment })
-          : httpServerless.post<XummPostPayloadResponse>("api/shop/xaman/signin");
+          : await axios.post<XummPostPayloadResponse>(`${Base.backendBaseUrl || `http://localhost:${process.env.SORCEL_DEV_BACKEND_PORT}`}/api/xumm/signin`);
 
         const { data } = await request;
         if (state.deepLink)
